1d6d158733a25f0402e830550a95116bf5e5b3d8
sobhanbera <sobhanbera258@gmail.com>
2021-06-18 22:15:30 +0530

    followings 1. app not configured 2. ios not needed 3. new package.json config 4. new tsconfig 5. only yarn.lock file remaining now.

--- App.tsx
+++ App.tsx
@@ -1,19 +1,61 @@
-import {Scaler} from './app/components'
-import React from 'react'
-import {SafeAreaView, StatusBar, Text} from 'react-native'
+import React, {useEffect, useState} from 'react'
+import {
+    FlatList,
+    Image,
+    SafeAreaView,
+    StatusBar,
+    Text,
+    TextInput,
+} from 'react-native'
+import axios, {CancelTokenSource} from 'axios'
+import SearchMusicType from 'app/api/types'
 
 const App = () => {
+    const [search, setSearch] = useState('')
+    const [searchResults, setSearchResults] = useState([])
+
+    useEffect(() => {
+        fetch('http://192.168.43.184:8000', {
+            headers: {
+                query: search,
+            },
+        })
+            .then(res => res.json())
+            .then(res => {
+                setSearchResults(res.content)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }, [search])
+
     return (
         <SafeAreaView>
             <StatusBar backgroundColor="black" barStyle="light-content" />
-            <Scaler>
-                <Text
-                    style={{
-                        color: 'black',
-                    }}>
-                    s
-                </Text>
-            </Scaler>
+
+            <TextInput
+                style={{
+                    backgroundColor: '#efefef',
+                    padding: 10,
+                    fontSize: 20,
+                }}
+                onChangeText={text => setSearch(text)}
+                value={search}
+                placeholder="Insert your text!"
+            />
+
+            {/* {searchResults.length > 0 ? ( */}
+            <FlatList
+                data={searchResults}
+                renderItem={item => {
+                    const realitem: SearchMusicType = item.item
+
+                    return (
+                        <Text style={{color: '#272727'}}>{realitem.name}</Text>
+                    )
+                }}
+            />
+            {/* ) : null} */}
         </SafeAreaView>
     )
 }
