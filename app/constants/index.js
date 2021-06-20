bbb6e639de0f0e9874aaa60ae69f565a77dcd132
sobhanbera <sobhanbera258@gmail.com>
2021-06-20 15:06:52 +0530

    added initial constants

---
+++ app/constants/index.js
@@ -0,0 +1,10 @@
+/** App details */
+export const APP_NAME = 'Sobyte'
+export const APP_DESCRIPTION = 'Free Music streamer and download.'
+
+/** Author Details */
+export const AUTHOR_NAME = 'Sobhan Bera'
+export const AUTHOR_NAME_LOWER = 'sobhanbera'
+
+/** Constants */
+export const STORAGE_KEY = '@APP:languageCode' // Storage key where the app language variable exists, for internationlization purpose
