e0cc085f45f706f3d19d8c26f71e550d592064e2
sobhanbera <sobhanbera258@gmail.com>
2021-06-20 15:07:52 +0530

    app details

---
+++ app.data.details.ts
@@ -0,0 +1,21 @@
+export default {
+    name: 'Sobyte',
+    displayName: 'Sobyte',
+    author: 'Sobhan Bera',
+    /** @version = String( @vc + "." + @vn ) */
+    version: 0.1,
+    /** @vc - version code */
+    vc: 0,
+    /** @vn - version number */
+    vn: 1,
+}
+
+// according to this details if a user has downloaded
+// a previous version of sobyte then a update prompt will be shown
+
+/**
+ * @if @vc < @newvc or @vn < @newvn then:
+ *      showAPromptForUpdaingTheApp()
+ * @newvc and @newvn would be feathed from the server
+ * whenever the new version of app is available
+ */
