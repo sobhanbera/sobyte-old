323b32025fd03a2b43d8676ee5f9b97c20fc80f8
sobhanbera <sobhanbera258@gmail.com>
2021-06-20 15:07:37 +0530

    initiated i18n

---
+++ app/i18n/index.js
@@ -0,0 +1,45 @@
+import i18n from 'i18next'
+import {initReactI18next} from 'react-i18next'
+import locale from 'react-native-locale-detector'
+import AsyncStorage from '@react-native-community/async-storage'
+
+import en from './en.json'
+import hi from './hi.json'
+
+import {STORAGE_KEY} from '../constants'
+
+/**
+ * locale system language detector
+ */
+const languageDetector = {
+    init: Function.prototype,
+    type: 'languageDetector',
+    async: true, // flags below detection to be async
+    detect: async callback => {
+        const savedDataJSON = await AsyncStorage.getItem(STORAGE_KEY)
+        const lng = savedDataJSON ? savedDataJSON : null
+        const selectLanguage = lng || locale
+        console.log('Language Detected:: ', selectLanguage)
+        callback(selectLanguage)
+    },
+    cacheUserLanguage: () => {},
+}
+
+i18n.use(languageDetector)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: 'en',
+        react: {
+            useSuspense: false,
+        },
+        resources: {en, hi},
+
+        ns: ['common'],
+        defaultNS: 'common',
+
+        interpolation: {
+            escapeValue: false, // not needed for react as it does escape per default to prevent xss!
+        },
+    })
+
+export default i18n
