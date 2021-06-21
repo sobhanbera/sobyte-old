2f3f9593595656ec281e9692175ed506e253bada
sobhanbera <sobhanbera258@gmail.com>
2021-06-21 09:32:10 +0530

    rest

--- App.tsx
+++ App.tsx
@@ -1,16 +1,6 @@
 import React from 'react'
-import {SafeAreaView, StatusBar, Text} from 'react-native'
-
 import MainApp from './app/MainApp'
 
-const App = () => {
-    return (
-        <SafeAreaView>
-            <StatusBar backgroundColor="black" barStyle="light-content" />
-
-            <MainApp />
-        </SafeAreaView>
-    )
-}
+const App = () => <MainApp />
 
 export default App
