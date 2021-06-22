474af5eb0907383f3abead3d5da1cc44b4646a4e
sobhanbera <sobhanbera258@gmail.com>
2021-06-22 10:29:20 +0530

    new global styles added

---
+++ app/styles/global.styles.ts
@@ -0,0 +1,16 @@
+import {FontFocusDisplay} from '../constants'
+import {StyleSheet} from 'react-native'
+
+const globalStyles = StyleSheet.create({
+    whiteText: {
+        color: 'white',
+    },
+    blackText: {
+        color: 'black',
+    },
+    textFontFocusDisplay: {
+        fontFamily: FontFocusDisplay,
+    },
+})
+
+export default globalStyles
