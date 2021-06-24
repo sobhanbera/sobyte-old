f80e5d62eab9503696a61618abeb7384218ed21b
sobhanbera <sobhanbera258@gmail.com>
2021-06-24 20:57:37 +0530

    linking of packages

--- android/app/src/main/java/com/sobyte/MainApplication.java
+++ android/app/src/main/java/com/sobyte/MainApplication.java
@@ -4,8 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.cmcewen.blurview.BlurViewPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
