cff7dc8c95075bee3781d1ae1a4efdde3fdb7c07
sobhanbera <sobhanbera258@gmail.com>
2021-06-25 20:48:22 +0530

    just a testing purpose this file will contain all the error log and there solution if any...

---
+++ app/rules/index.ts
@@ -0,0 +1,4 @@
+/** @animation - Invariant Violation: You can define one of bounciness/speed, tension/friction,
+ *  or stiffness/damping/mass,
+ *  but not more than one */
+export default 'this is not a module'
