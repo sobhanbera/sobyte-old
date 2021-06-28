896802701acaeef57dcb7fe9797a54f9ccc81c01
sobhanbera <sobhanbera258@gmail.com>
2021-06-28 20:58:38 +0530

    context update

--- app/context/index.ts
+++ app/context/index.ts
@@ -1,11 +1,12 @@
 import {useMusicApi} from '../api'
 import {useApp} from '../MainApp'
 import {useTheme} from '../themes/ThemeProvider'
+import {useFetcher} from '../api/MusicFetcher'
 
 import Player, {usePlayer} from '../api/PlayerControls'
 
 export default Player
-export {useTheme, useApp, useMusicApi, usePlayer}
+export {useTheme, useApp, useFetcher, useMusicApi, usePlayer}
 
 /** external apis */
 import {useTrackPlayerProgress} from 'react-native-track-player'
