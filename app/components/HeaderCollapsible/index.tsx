06415538751e55df87a00db06d52c87dd5f1877b
sobhanbera <sobhanbera258@gmail.com>
2021-06-30 22:40:51 +0530

    new collapsible header component

---
+++ app/components/HeaderCollapsible/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {StyleSheet, Animated, ImageBackground} from 'react-native'
+
+interface Props {
+    headerScrollHeight: any
+}
+const HeaderCollapsible: React.FC<Props> = props => {
+    return (
+        <Animated.View
+            style={{
+                position: 'absolute',
+                left: 0,
+                right: 0,
+                top: 0,
+                height: props.headerScrollHeight,
+                width: '100%',
+                overflow: 'hidden',
+                zIndex: 999,
+                backgroundColor: 'blue',
+            }}>
+            <ImageBackground
+                style={{flex: 1}}
+                source={require('../../assets/images/phone_screen.png')}>
+                {props.children}
+            </ImageBackground>
+        </Animated.View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default HeaderCollapsible
