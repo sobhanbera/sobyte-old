76346e581ae3e306e71248529b88c848d3275306
sobhanbera <sobhanbera258@gmail.com>
2021-06-30 22:42:15 +0530

    not done yet

--- app/screens/main/Explore/Explore.tsx
+++ app/screens/main/Explore/Explore.tsx
@@ -1,10 +1,65 @@
-import React from 'react'
-import {Text, View} from 'react-native'
-import {usePlayer} from '../../../context'
+import React, {useRef} from 'react'
+import {Animated, Image, ScrollView, Text, View} from 'react-native'
 
-interface ExploreProps {}
-const Explore: React.FC<ExploreProps> = props => {
-    return <View></View>
+import {
+    GradientBackground,
+    HeaderCollapsible,
+    HeaderSearch,
+} from '../../../components'
+import {
+    HEADER_MAX_HEIGHT,
+    HEADER_MIN_HEIGHT,
+    HEADER_SCROLL_DISTANCE,
+} from '../../../constants'
+
+interface ProfileProps {
+    navigation?: any
+}
+const Profile: React.FC<ProfileProps> = props => {
+    const scrollOffsetY = useRef(new Animated.Value(0)).current
+    const headerScrollHeight = scrollOffsetY.interpolate({
+        inputRange: [0, HEADER_SCROLL_DISTANCE],
+        outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+        extrapolate: 'clamp',
+    })
+
+    return (
+        <GradientBackground>
+            <HeaderCollapsible headerScrollHeight={headerScrollHeight} />
+
+            <HeaderSearch />
+
+            <ScrollView
+                onScroll={Animated.event([
+                    {nativeEvent: {contentOffset: {y: scrollOffsetY}}},
+                ])}
+                scrollEventThrottle={16}>
+                <View style={{paddingTop: HEADER_MAX_HEIGHT}}>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                    <Text style={{fontSize: 40}}>AKJSHDKJAD</Text>
+                </View>
+            </ScrollView>
+        </GradientBackground>
+    )
 }
 
-export default Explore
+export default Profile
