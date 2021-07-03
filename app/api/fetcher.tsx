9679aedd97a97cbf609f6a4f1117dfddec17548b
sobhanbera <sobhanbera258@gmail.com>
2021-07-03 15:09:45 +0530

    all functions are completed in function component of music provider context api

--- app/api/fetcher.tsx
+++ app/api/fetcher.tsx
@@ -6,29 +6,30 @@ import _ from 'lodash'
 
 import * as utils from './utils'
 import * as parsers from './parsers'
+import {ContinuationObjectItself, ContinuationObject} from '../interfaces'
 
 const DemoMusicContextReturn = () => new Promise(() => {})
 const MusicContext = React.createContext({
-    // initalize: () => DemoMusicContextReturn(),
-    // getContinuation: (
-    //     endpointName = 'song',
-    //     continuation = {
-    //         continuation: '',
-    //         clickTrackingParams: '',
-    //     },
-    // ) => DemoMusicContextReturn(),
-    // getSearchSuggestions: (search = '') => DemoMusicContextReturn(),
+    initalize: () => DemoMusicContextReturn(),
+    getContinuation: (
+        _endpointName: string,
+        _continuation: ContinuationObject,
+    ) => DemoMusicContextReturn(),
+    getSearchSuggestions: (_search: string) => DemoMusicContextReturn(),
     search: (
-        query: string,
-        categoryName: string = 'song',
+        _query: string,
+        _categoryName: string = 'song',
         _pageLimit: number = 1,
     ) => DemoMusicContextReturn(),
-    // getAlbum: (browseId = '') => DemoMusicContextReturn(),
-    // getPlaylist: (browseId = '', contentLimit = 100) =>
-    //     DemoMusicContextReturn(),
-    // getArtist: (browseId = '') => DemoMusicContextReturn(),
-    // getNext: (videoId = '', playlistId = '', paramString = '') =>
-    //     DemoMusicContextReturn(),
+    getAlbum: (_browseId: string) => DemoMusicContextReturn(),
+    getPlaylist: (_browseId: string, _contentLimit = 100) =>
+        DemoMusicContextReturn(),
+    getArtist: (_browseId: string) => DemoMusicContextReturn(),
+    getNext: (_musicId: string, _playlistId: string, _paramString: string) =>
+        DemoMusicContextReturn(),
+
+    error: false,
+    loaded: false,
 })
 
 interface MusicApiProps {
@@ -150,6 +151,59 @@ const MusicApi = (props: MusicApiProps) => {
         })
     }
 
+    const getContinuation = (
+        endpointName: string,
+        continuation: ContinuationObject,
+    ) => {
+        if (
+            // continuation != [] &&
+            continuation instanceof Object &&
+            continuation.continuation &&
+            continuation.clickTrackingParams
+        ) {
+            return new Promise((resolve, reject) => {
+                _createApiRequest(
+                    endpointName,
+                    {},
+                    {
+                        ctoken: continuation.continuation,
+                        continuation: continuation.continuation,
+                        itct: continuation.clickTrackingParams,
+                        type: 'next',
+                    },
+                ).then(context => {
+                    // let parse:Date = new Date()
+                    let r = parsers.parseSongSearchResult(context)
+                    // let o:number = new Date() - parse
+                    resolve(r)
+                })
+            })
+        } else {
+            return new Promise(() => {})
+        }
+    }
+
+    const getSearchSuggestions = (query: string) => {
+        return new Promise((resolve, reject) => {
+            _createApiRequest('music/get_search_suggestions', {
+                input: query,
+            })
+                .then(content => {
+                    try {
+                        resolve(
+                            utils.fv(
+                                content,
+                                'searchSuggestionRenderer:navigationEndpoint:query',
+                            ),
+                        )
+                    } catch (error) {
+                        reject(error)
+                    }
+                })
+                .catch(error => reject(error))
+        })
+    }
+
     const search = (
         query: string,
         categoryName: string = 'song',
@@ -196,8 +250,175 @@ const MusicApi = (props: MusicApiProps) => {
         })
     }
 
+    const getAlbum = (browseId: string) => {
+        if (_.startsWith(browseId, 'MPREb')) {
+            return new Promise((resolve, reject) => {
+                _createApiRequest(
+                    'browse',
+                    utils.buildEndpointContext('ALBUM', browseId),
+                )
+                    .then(context => {
+                        try {
+                            const result = parsers.parseAlbumPage(context)
+                            resolve(result)
+                        } catch (error) {
+                            return resolve({
+                                error: error.message,
+                            })
+                        }
+                    })
+                    .catch(error => reject(error))
+            })
+        } else {
+            throw new Error('invalid album browse id.')
+        }
+    }
+
+    const getPlaylist = (browseId: string, contentLimit = 100) => {
+        if (_.startsWith(browseId, 'VL') || _.startsWith(browseId, 'PL')) {
+            _.startsWith(browseId, 'PL') && (browseId = 'VL' + browseId)
+            return new Promise((resolve, reject) => {
+                _createApiRequest(
+                    'browse',
+                    utils.buildEndpointContext('PLAYLIST', browseId),
+                )
+                    .then(context => {
+                        try {
+                            var result = parsers.parsePlaylistPage(context)
+                            const getContinuations = (
+                                params: ContinuationObjectItself,
+                            ) => {
+                                _createApiRequest(
+                                    'browse',
+                                    {},
+                                    {
+                                        ctoken: params.continuation,
+                                        continuation: params.continuation,
+                                        itct: params.continuation
+                                            .clickTrackingParams,
+                                    },
+                                ).then(context => {
+                                    const continuationResult =
+                                        parsers.parsePlaylistPage(context)
+                                    if (
+                                        Array.isArray(
+                                            continuationResult.content,
+                                        )
+                                    ) {
+                                        result.content = _.concat(
+                                            result.content,
+                                            continuationResult.content,
+                                        )
+                                        result.continuation =
+                                            continuationResult.continuation
+                                    }
+                                    if (
+                                        !Array.isArray(
+                                            continuationResult.continuation,
+                                        ) &&
+                                        result.continuation instanceof Object
+                                    ) {
+                                        if (
+                                            contentLimit > result.content.length
+                                        ) {
+                                            getContinuations(
+                                                continuationResult.continuation,
+                                            )
+                                        } else {
+                                            return resolve(result)
+                                        }
+                                    } else {
+                                        return resolve(result)
+                                    }
+                                })
+                            }
+
+                            if (
+                                contentLimit > result.content.length &&
+                                !Array.isArray(result.continuation) &&
+                                result.continuation instanceof Object
+                            ) {
+                                getContinuations(result.continuation)
+                            } else {
+                                return resolve(result)
+                            }
+                        } catch (error) {
+                            return resolve({
+                                error: error.message,
+                            })
+                        }
+                    })
+                    .catch(error => reject(error))
+            })
+        } else {
+            throw new Error('invalid playlist id.')
+        }
+    }
+
+    const getArtist = (browseId: string) => {
+        if (_.startsWith(browseId, 'UC')) {
+            return new Promise((resolve, reject) => {
+                _createApiRequest(
+                    'browse',
+                    utils.buildEndpointContext('ARTIST', browseId),
+                )
+                    .then(context => {
+                        try {
+                            const result = parsers.parseArtistPage(context)
+                            resolve(result)
+                        } catch (error) {
+                            resolve({
+                                error: error.message,
+                            })
+                        }
+                    })
+                    .catch(error => reject(error))
+            })
+        } else {
+            throw new Error('invalid artist browse id.')
+        }
+    }
+
+    const getNext = (
+        musicId: string,
+        playlistId: string,
+        paramString: string,
+    ) => {
+        return new Promise((resolve, reject) => {
+            _createApiRequest('next', {
+                enablePersistentPlaylistPanel: true,
+                isAudioOnly: true,
+                params: paramString,
+                playlistId: playlistId,
+                tunerSettingValue: 'AUTOMIX_SETTING_NORMAL',
+                videoId: musicId,
+            })
+                .then(context => {
+                    try {
+                        const result = parsers.parseNextPanel(context)
+                        resolve(result)
+                    } catch (error) {
+                        resolve({
+                            error: error.message,
+                        })
+                    }
+                })
+                .catch(error => reject(error))
+        })
+    }
+
     const musicApiValues = {
+        initalize: initalize,
+        getContinuation: getContinuation,
+        getSearchSuggestions: getSearchSuggestions,
         search: search,
+        getAlbum: getAlbum,
+        getPlaylist: getPlaylist,
+        getArtist: getArtist,
+        getNext: getNext,
+
+        error: error,
+        loaded: loaded,
     }
     return (
         <MusicContext.Provider value={musicApiValues}>
