bcc88aad87e99dda9d558705cccccf3d71fca2f2
sobhanbera <sobhanbera258@gmail.com>
2021-07-03 15:23:46 +0530

    added all comments in the musicApi component

--- app/api/fetcher.tsx
+++ app/api/fetcher.tsx
@@ -1,12 +1,12 @@
-import axios from 'axios'
-import React, {useEffect, useState} from 'react'
-import RNLocalize from 'react-native-localize'
-import querystring from 'querystring'
-import _ from 'lodash'
+import React, {useEffect, useState} from 'react' // as usual import
+import axios from 'axios' // for making api requests
+import RNLocalize from 'react-native-localize' // to pass the location of user in the api call
+import querystring from 'querystring' // string methods
+import _ from 'lodash' // util methods
 
-import * as utils from './utils'
-import * as parsers from './parsers'
-import {ContinuationObjectItself, ContinuationObject} from '../interfaces'
+import * as utils from './utils' // local util path
+import * as parsers from './parsers' // local parser path
+import {ContinuationObjectItself, ContinuationObject} from '../interfaces' // objects required for instances in this files
 
 const DemoMusicContextReturn = () => new Promise(() => {})
 const MusicContext = React.createContext({
@@ -36,10 +36,16 @@ interface MusicApiProps {
     children: React.ReactChild
 }
 const MusicApi = (props: MusicApiProps) => {
+    /**
+     * state
+     */
     const [musicConfig, setMusicConfig] = useState<{[key: string]: string}>({})
     const [error, setError] = useState(false)
     const [loaded, setLoaded] = useState(false)
 
+    /**
+     * main and core required variable to make requests to the backend
+     */
     let fetchConfig: {[key: string]: string} = {}
     let client = axios.create({
         baseURL: 'https://music.youtube.com/',
@@ -51,6 +57,12 @@ const MusicApi = (props: MusicApiProps) => {
         withCredentials: true,
     })
 
+    /**
+     *
+     * @returns a promise after initializing the music api fetcher this
+     * function should be called in the root element of the app where the
+     * main app starts after using this function context provider
+     */
     const initalize = () => {
         return new Promise((resolve, reject) => {
             client
@@ -91,10 +103,15 @@ const MusicApi = (props: MusicApiProps) => {
         })
     }
 
-    useEffect(() => {
-        initalize()
-    }, [])
-
+    /**
+     *
+     * @param endpointName the name of api endpoint where the call is to be made
+     * @param inputVariables all data which should be POST to the endpoint
+     * @param inputQuery extra data to provide while calling api endpoint
+     * @returns everyfunction that exists in this component and makes api
+     * call to the backend it will require to call this function as this would
+     * be the wrapper function of all of them...
+     */
     const _createApiRequest = (
         endpointName: string,
         inputVariables: Object,
@@ -151,6 +168,12 @@ const MusicApi = (props: MusicApiProps) => {
         })
     }
 
+    /**
+     *
+     * @param endpointName this is the endpoint name like "search" or anything else
+     * @param continuation this is the continuation object which is provided in the fetched data after making any request to get more data of the same type...
+     * @returns return the same type of object for which the continuation is provided
+     */
     const getContinuation = (
         endpointName: string,
         continuation: ContinuationObject,
@@ -183,6 +206,11 @@ const MusicApi = (props: MusicApiProps) => {
         }
     }
 
+    /**
+     *
+     * @param query the query string for getting suggestions
+     * @returns object with array of strings containing the search suggestion...
+     */
     const getSearchSuggestions = (query: string) => {
         return new Promise((resolve, reject) => {
             _createApiRequest('music/get_search_suggestions', {
@@ -204,6 +232,13 @@ const MusicApi = (props: MusicApiProps) => {
         })
     }
 
+    /**
+     *
+     * @param query the query string
+     * @param categoryName what type of data is needed like "song" | "album" | "playlist"
+     * @param _pageLimit number of data page wise
+     * @returns the search result after making api request
+     */
     const search = (
         query: string,
         categoryName: string = 'song',
@@ -250,6 +285,11 @@ const MusicApi = (props: MusicApiProps) => {
         })
     }
 
+    /**
+     *
+     * @param browseId id of the album
+     * @returns the object with album data
+     */
     const getAlbum = (browseId: string) => {
         if (_.startsWith(browseId, 'MPREb')) {
             return new Promise((resolve, reject) => {
@@ -274,6 +314,12 @@ const MusicApi = (props: MusicApiProps) => {
         }
     }
 
+    /**
+     *
+     * @param browseId id of the playlist
+     * @param contentLimit limiting the data
+     * @returns the object with playlist data
+     */
     const getPlaylist = (browseId: string, contentLimit = 100) => {
         if (_.startsWith(browseId, 'VL') || _.startsWith(browseId, 'PL')) {
             _.startsWith(browseId, 'PL') && (browseId = 'VL' + browseId)
@@ -355,6 +401,11 @@ const MusicApi = (props: MusicApiProps) => {
         }
     }
 
+    /**
+     *
+     * @param browseId id of the artist
+     * @returns the object with artist data
+     */
     const getArtist = (browseId: string) => {
         if (_.startsWith(browseId, 'UC')) {
             return new Promise((resolve, reject) => {
@@ -379,6 +430,13 @@ const MusicApi = (props: MusicApiProps) => {
         }
     }
 
+    /**
+     *
+     * @param musicId id of the music
+     * @param playlistId id of the playlist
+     * @param paramString id of the param string if any
+     * @returns the object with songs list in that particular playlist
+     */
     const getNext = (
         musicId: string,
         playlistId: string,
@@ -407,6 +465,9 @@ const MusicApi = (props: MusicApiProps) => {
         })
     }
 
+    /**
+     * music api context values provider
+     */
     const musicApiValues = {
         initalize: initalize,
         getContinuation: getContinuation,
@@ -427,5 +488,8 @@ const MusicApi = (props: MusicApiProps) => {
     )
 }
 
+/**
+ * exports the main component and the api for using the MusicApi context
+ */
 export default MusicApi
 export const useMusicApi = () => React.useContext(MusicContext)
