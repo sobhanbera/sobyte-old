81d21f6a6eff1d6fdffa6f517a42f02ca3c87251
sobhanbera <sobhanbera258@gmail.com>
2021-07-03 16:51:03 +0530

    nothing to see

--- App.tsx
+++ App.tsx
@@ -4,3 +4,8 @@ import MainApp from './app/MainApp'
 const App = () => <MainApp />
 
 export default App
+
+// console.log(
+//     '%cWelcome to sobyte development mode...',
+//     'background: black; color: lightblue; padding: 10px; margin: 10px; border-radius: 6px; border: 1px solid grey;',
+// )
