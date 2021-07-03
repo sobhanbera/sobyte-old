c64d298d6bf1d8d8e1384488c038305f3aa4ddea
sobhanbera <sobhanbera258@gmail.com>
2021-07-03 23:52:45 +0530

    new package react-native-reanimated@next is added

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-};
+    presets: ['module:metro-react-native-babel-preset'],
+
+    /** 'react-native-reanimated/plugin' should be placed last in the plugins list */
+    plugins: ['react-native-reanimated/plugin'],
+}
