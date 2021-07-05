620378295e494d88e88f2d6327f66a95d1aa6d9a
sobhanbera <sobhanbera258@gmail.com>
2021-07-05 23:49:08 +0530

    more usable now

--- app/components/HeaderSearch/index.tsx
+++ app/components/HeaderSearch/index.tsx
@@ -4,11 +4,12 @@ import Entypo from 'react-native-vector-icons/Entypo'
 
 import {useTheme} from '../../context'
 import globalStyles from '../../styles/global.styles'
-import {DEFAULT_SMALL_ICON_SIZE} from '../../constants'
+import {DEFAULT_SMALL_ICON_SIZE, HEADER_MIN_HEIGHT} from '../../constants'
 
 interface Props {
     text: string
     onChangeText: Function
+    onSubmit: Function
 }
 const HeaderSearch: React.FC<Props> = props => {
     const {white, black, surfacelight, border, text, primary, placeholder} =
@@ -20,8 +21,8 @@ const HeaderSearch: React.FC<Props> = props => {
                 globalStyles.header,
                 {
                     backgroundColor: surfacelight[0],
-                    borderBottomColor: border[0],
-                    borderBottomWidth: 0.5,
+                    borderBottomColor: black[0] + '40',
+                    borderBottomWidth: 1,
                 },
             ]}>
             <Entypo
@@ -34,7 +35,6 @@ const HeaderSearch: React.FC<Props> = props => {
                 value={props.text}
                 onChangeText={e => props.onChangeText(e)}
                 selectionColor={primary.dark[0]}
-                keyboardType="numeric"
                 placeholderTextColor={placeholder[0]}
                 style={{
                     flex: 1,
@@ -44,7 +44,9 @@ const HeaderSearch: React.FC<Props> = props => {
                     backgroundColor: black[0] + '40',
                     paddingHorizontal: 15,
                     borderRadius: 100,
+                    height: HEADER_MIN_HEIGHT - 12,
                 }}
+                onSubmitEditing={() => props.onSubmit()}
             />
         </View>
     )
