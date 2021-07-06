97166ae3cbf100832c42534a3b33233240a2909f
sobhanbera <sobhanbera258@gmail.com>
2021-07-07 00:21:46 +0530

    updated modals

--- app/interfaces/Data.ts
+++ app/interfaces/Data.ts
@@ -4,7 +4,7 @@ export const MoodCategories: Array<SongCategory> = [
     {
         id: '1',
         name: 'Cheerful',
-        image: 'https://images.unsplash.com/photo-1509909756405-be0199881695?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aGFwcHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1509909756405-be0199881695?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aGFwcHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         highimage:
             'https://images.unsplash.com/photo-1509909756405-be0199881695?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aGFwcHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=100',
         color: ['#F39A48', '#F39A48'],
@@ -12,7 +12,7 @@ export const MoodCategories: Array<SongCategory> = [
     {
         id: '2',
         name: 'Romantic',
-        image: 'https://images.unsplash.com/photo-1496062031456-07b8f162a322?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cm9zZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1496062031456-07b8f162a322?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cm9zZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         highimage:
             'https://images.unsplash.com/photo-1496062031456-07b8f162a322?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cm9zZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=100',
         color: ['#CC353D', '#CC353D'],
@@ -20,7 +20,7 @@ export const MoodCategories: Array<SongCategory> = [
     {
         id: '3',
         name: 'Party',
-        image: 'https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGFydHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGFydHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         highimage:
             'https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGFydHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=100',
         color: ['#000000', '#000000'],
@@ -28,7 +28,7 @@ export const MoodCategories: Array<SongCategory> = [
     {
         id: '4',
         name: 'Bored',
-        image: 'https://images.unsplash.com/photo-1586473219010-2ffc57b0d282?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Ym9yZWR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1586473219010-2ffc57b0d282?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Ym9yZWR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         highimage:
             'https://images.unsplash.com/photo-1586473219010-2ffc57b0d282?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Ym9yZWR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=100',
         color: ['#935128', '#935128'],
@@ -36,7 +36,7 @@ export const MoodCategories: Array<SongCategory> = [
     {
         id: '5',
         name: 'Travel',
-        image: 'https://images.unsplash.com/photo-1488646953014-85cb44e25828?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dHJhdmVsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1488646953014-85cb44e25828?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dHJhdmVsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         highimage:
             'https://images.unsplash.com/photo-1488646953014-85cb44e25828?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dHJhdmVsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=100',
         color: ['#458563', '#458563'],
@@ -44,7 +44,7 @@ export const MoodCategories: Array<SongCategory> = [
     {
         id: '6',
         name: 'Relax',
-        image: 'https://images.unsplash.com/photo-1506126613408-eca07ce68773?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHJlbGF4fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1506126613408-eca07ce68773?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHJlbGF4fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         highimage:
             'https://images.unsplash.com/photo-1506126613408-eca07ce68773?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHJlbGF4fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=100',
         color: ['#A387EE', '#A387EE'],
@@ -52,7 +52,7 @@ export const MoodCategories: Array<SongCategory> = [
     {
         id: '7',
         name: 'Sad',
-        image: 'https://images.unsplash.com/photo-1620063633168-8b30e52edf95?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FkJTIwZW1vaml8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1620063633168-8b30e52edf95?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FkJTIwZW1vaml8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         highimage:
             'https://images.unsplash.com/photo-1620063633168-8b30e52edf95?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FkJTIwZW1vaml8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=100',
         color: ['#272727', '#272727'],
@@ -60,7 +60,7 @@ export const MoodCategories: Array<SongCategory> = [
     {
         id: '8',
         name: 'Angry',
-        image: 'https://images.unsplash.com/photo-1503525148566-ef5c2b9c93bd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGFuZ3J5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1503525148566-ef5c2b9c93bd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGFuZ3J5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         highimage:
             'https://images.unsplash.com/photo-1503525148566-ef5c2b9c93bd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGFuZ3J5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=100',
         color: ['#FF0000', '#FF0000'],
@@ -68,7 +68,7 @@ export const MoodCategories: Array<SongCategory> = [
     {
         id: '9',
         name: 'Lo-Fi',
-        image: 'https://images.unsplash.com/photo-1615554851544-e6249b92a492?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bG8lMjBmaSUyMG11c2ljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1615554851544-e6249b92a492?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bG8lMjBmaSUyMG11c2ljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         highimage:
             'https://images.unsplash.com/photo-1615554851544-e6249b92a492?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bG8lMjBmaSUyMG11c2ljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=100',
         color: ['#EE537E', '#EE537E'],
@@ -79,7 +79,7 @@ export const GenresCategories: Array<SongCategory> = [
     {
         id: '101',
         name: 'Electronic',
-        image: 'https://images.unsplash.com/photo-1535979014625-490762ceb2ff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZWxlY3Ryb25pYyUyMG11c2ljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1535979014625-490762ceb2ff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZWxlY3Ryb25pYyUyMG11c2ljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         highimage:
             'https://images.unsplash.com/photo-1535979014625-490762ceb2ff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZWxlY3Ryb25pYyUyMG11c2ljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=100',
         color: ['#D7CECF', '#D7CECF'],
@@ -87,7 +87,7 @@ export const GenresCategories: Array<SongCategory> = [
     {
         id: '102',
         name: 'Hip-Hop',
-        image: 'https://images.unsplash.com/photo-1601643157091-ce5c665179ab?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGlwJTIwaG9wJTIwbXVzaWN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1601643157091-ce5c665179ab?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGlwJTIwaG9wJTIwbXVzaWN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         highimage:
             'https://images.unsplash.com/photo-1601643157091-ce5c665179ab?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGlwJTIwaG9wJTIwbXVzaWN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=100',
         color: ['#1B2E6E', '#1B2E6E'],
@@ -95,7 +95,7 @@ export const GenresCategories: Array<SongCategory> = [
     {
         id: '103',
         name: 'Pop',
-        image: 'https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cG9wJTIwbXVzaWN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cG9wJTIwbXVzaWN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         highimage:
             'https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cG9wJTIwbXVzaWN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=100',
         color: ['#E7697A', '#E7697A'],
@@ -103,15 +103,15 @@ export const GenresCategories: Array<SongCategory> = [
     {
         id: '104',
         name: 'Folk',
-        image: 'https://images.unsplash.com/photo-1508026119240-df332d6feed1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9sayUyMG11c2ljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1508026119240-df332d6feed1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9sayUyMG11c2ljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         highimage:
-            'https://images.unsplash.com/photo-1508026119240-df332d6feed1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9sayUyMG11c2ljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+            'https://images.unsplash.com/photo-1508026119240-df332d6feed1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9sayUyMG11c2ljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         color: ['#E7B361', '#E7B361'],
     },
     {
         id: '105',
         name: 'Jazz',
-        image: 'https://images.unsplash.com/photo-1513829596324-4bb2800c5efb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8amF6enxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1513829596324-4bb2800c5efb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8amF6enxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=175&q=60',
         highimage:
             'https://images.unsplash.com/photo-1513829596324-4bb2800c5efb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8amF6enxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=720&q=100',
         color: ['#458563', '#458563'],
@@ -119,7 +119,7 @@ export const GenresCategories: Array<SongCategory> = [
     {
         id: '106',
         name: 'Metal',
-        image: 'https://images.unsplash.com/photo-1527624111800-38e9283e7470?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fG1ldGFsJTIwbXVzaWN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1527624111800-38e9283e7470?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fG1ldGFsJTIwbXVzaWN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         highimage:
             'https://images.unsplash.com/photo-1527624111800-38e9283e7470?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fG1ldGFsJTIwbXVzaWN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=100',
         color: ['#082529', '#082529'],
@@ -127,7 +127,7 @@ export const GenresCategories: Array<SongCategory> = [
     {
         id: '107',
         name: 'Rock',
-        image: 'https://images.unsplash.com/photo-1604364505966-e35d40abb1ae?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cm9jayUyMG11c2ljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1604364505966-e35d40abb1ae?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cm9jayUyMG11c2ljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         highimage:
             'https://images.unsplash.com/photo-1604364505966-e35d40abb1ae?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cm9jayUyMG11c2ljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=100',
         color: ['#FFA500', '#FFA500'],
@@ -135,15 +135,15 @@ export const GenresCategories: Array<SongCategory> = [
     {
         id: '108',
         name: 'Classical',
-        image: 'https://images.unsplash.com/photo-1519683109079-d5f539e1542f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2xhc3NpY2FsJTIwbXVzaWN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1519683109079-d5f539e1542f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2xhc3NpY2FsJTIwbXVzaWN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         highimage:
-            'https://images.unsplash.com/photo-1519683109079-d5f539e1542f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2xhc3NpY2FsJTIwbXVzaWN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+            'https://images.unsplash.com/photo-1519683109079-d5f539e1542f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2xhc3NpY2FsJTIwbXVzaWN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         color: ['#376977', '#376977'],
     },
     {
         id: '109',
         name: 'R&B',
-        image: 'https://images.unsplash.com/photo-1477233534935-f5e6fe7c1159?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ciUyNmIlMjBtdXNpY3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=100',
+        image: 'https://images.unsplash.com/photo-1477233534935-f5e6fe7c1159?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ciUyNmIlMjBtdXNpY3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=175&q=60',
         highimage:
             'https://images.unsplash.com/photo-1477233534935-f5e6fe7c1159?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ciUyNmIlMjBtdXNpY3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=100',
         color: ['#586066', '#586066'],
