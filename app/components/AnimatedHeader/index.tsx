de76e827c2bc61feb99c2838d794c7423c6fdc80
sobhanbera <sobhanbera258@gmail.com>
2021-07-07 22:17:51 +0530

    new component for animated header which is shifted from SongCategory to its own file

---
+++ app/components/AnimatedHeader/index.tsx
@@ -0,0 +1,166 @@
+import React from 'react'
+import {View, Text, Image, Dimensions, Pressable} from 'react-native'
+import ImageHeaderScrollView, {
+    TriggeringView,
+} from 'react-native-image-header-scroll-view'
+import {useTheme} from '../../context'
+import * as Animatable from 'react-native-animatable'
+
+import {SongCategory} from '../../interfaces'
+import {
+    HEADER_MAX_HEIGHT,
+    HEADER_MIN_HEIGHT,
+    PaddingBottomView,
+} from '../../constants'
+import globalStyles from '../../styles/global.styles'
+
+interface Props {
+    category: SongCategory
+    headerTitle: string
+}
+const AnimatedHeader = (props: Props) => {
+    const {themeColors} = useTheme()
+    const headerTitleReference = React.useRef<any>(null)
+
+    const showHeaderTitle = () => {
+        headerTitleReference.current?.slideInDown(500)
+    }
+    const hideHeaderTitle = () => {
+        headerTitleReference.current?.slideOutUp(500)
+    }
+
+    return (
+        <ImageHeaderScrollView
+            showsHorizontalScrollIndicator={false}
+            showsVerticalScrollIndicator={false}
+            bounces={false}
+            scrollViewBackgroundColor={themeColors.surfacelight[0]}
+            overlayColor={themeColors.surfacelight[0]}
+            maxOverlayOpacity={1}
+            minOverlayOpacity={0.4}
+            maxHeight={350}
+            minHeight={HEADER_MIN_HEIGHT}
+            headerImage={{
+                uri: props.category.highimage,
+            }}
+            renderHeader={() => (
+                <View
+                    style={[
+                        {
+                            backgroundColor: 'black',
+                        },
+                        globalStyles.lightBottomBorder,
+                    ]}>
+                    <Image
+                        source={{uri: props.category.highimage}}
+                        style={{
+                            height: HEADER_MAX_HEIGHT,
+                            width: Dimensions.get('window').width,
+                            alignSelf: 'stretch',
+                            resizeMode: 'cover',
+                        }}
+                    />
+                </View>
+            )}
+            renderForeground={() => {
+                return (
+                    <View
+                        style={[
+                            globalStyles.animatedHeaderTitleContainer,
+                            globalStyles.lightBottomBorder,
+                            {
+                                height: '100%',
+                            },
+                        ]}>
+                        <Pressable onPress={() => {}}>
+                            <Text style={globalStyles.animatedHeaderTitle}>
+                                {props.category.name}
+                            </Text>
+                        </Pressable>
+                    </View>
+                )
+            }}
+            renderFixedForeground={() => (
+                <Animatable.View
+                    style={[
+                        globalStyles.animatedHeaderTitleContainer,
+                        globalStyles.lightBottomBorder,
+                        {
+                            backgroundColor: themeColors.surfacelight[0],
+                            borderBottomWidth: 1,
+                        },
+                    ]}
+                    ref={headerTitleReference}>
+                    <Text
+                        style={[
+                            globalStyles.animatedHeaderTitle,
+                            {
+                                fontSize: 20,
+                            },
+                        ]}>
+                        {props.headerTitle}
+                    </Text>
+                </Animatable.View>
+            )}>
+            <TriggeringView
+                onBeginHidden={showHeaderTitle}
+                onHide={showHeaderTitle}
+                onBeginDisplayed={hideHeaderTitle}
+                onDisplay={hideHeaderTitle}>
+                <Text
+                    style={{
+                        fontSize: 100,
+                        color: 'white',
+                    }}>
+                    Scroll Me!
+                </Text>
+                <Text
+                    style={{
+                        fontSize: 100,
+                        color: 'white',
+                    }}>
+                    Scroll Me!
+                </Text>
+                <Text
+                    style={{
+                        fontSize: 100,
+                        color: 'white',
+                    }}>
+                    Scroll Me!
+                </Text>
+                <Text
+                    style={{
+                        fontSize: 100,
+                        color: 'white',
+                    }}>
+                    Scroll Me!
+                </Text>
+                <Text
+                    style={{
+                        fontSize: 100,
+                        color: 'white',
+                    }}>
+                    Scroll Me!
+                </Text>
+                <Text
+                    style={{
+                        fontSize: 100,
+                        color: 'white',
+                    }}>
+                    Scroll Me!
+                </Text>
+                <Text
+                    style={{
+                        fontSize: 100,
+                        color: 'white',
+                    }}>
+                    Scroll Me!
+                </Text>
+
+                <PaddingBottomView />
+            </TriggeringView>
+        </ImageHeaderScrollView>
+    )
+}
+
+export default AnimatedHeader
