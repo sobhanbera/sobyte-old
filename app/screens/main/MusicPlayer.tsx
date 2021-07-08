1cd52f2e0097346b772f103999ec02b09dd7132f
sobhanbera <sobhanbera258@gmail.com>
2021-07-08 22:07:25 +0530

    music player screen is paused for now

--- app/screens/main/MusicPlayer.tsx
+++ app/screens/main/MusicPlayer.tsx
@@ -1,40 +1,74 @@
 import React, {useEffect, useState} from 'react'
-import {ImageBackground, Text, View} from 'react-native'
+import {Image, ImageBackground, StyleSheet, Text, View} from 'react-native'
 
 import {DoubleTap, GradientBackground, ProgressSlider} from '../../components'
-import {usePlayer, usePrompt} from '../../context'
+import {usePlayer, usePrompt, useTheme} from '../../context'
 import globalStyles from '../../styles/global.styles'
 import {MUSIC_PLAYER_BLUR} from '../../constants'
+import {DominatingColors} from '../../interfaces'
+import ImageColors from 'react-native-image-colors'
 
 interface PlayerProps {
     navigation?: any
 }
 const Player: React.FC<PlayerProps> = props => {
     const {current, playonly} = usePlayer()
+    const {themeColors} = useTheme()
+
+    const [backgroundColor, setBackgroundColors] = useState<string[]>(
+        themeColors.backgroundgradient,
+    )
 
     useEffect(() => {
         props.navigation.addListener('focus', () => {
             playonly()
         })
-    }, [])
+
+        console.log(current.artwork)
+
+        if (current.artwork)
+            ImageColors.getColors(current.artwork, {
+                fallback: themeColors.primary.dark[0],
+                cache: true,
+                key: 'sobyte_music_player_color',
+            })
+                .then(res => {
+                    console.log(res)
+                })
+                .catch(err => {
+                    setBackgroundColors(themeColors.backgroundgradient)
+                })
+    }, [current.artwork])
 
     return (
-        <GradientBackground>
+        <GradientBackground colors={backgroundColor}>
+            {/* background image with blur */}
+            {/* <Image
+                source={{
+                    uri:
+                        current.artwork ||
+                        'https://lh3.googleusercontent.com/fx6kz_AXXbWVglzvmChcvFRTwV_nFmSAjuyYUO2XTqO_Es9hv4Fe6scz1pTfNWw6hSWob6gtpsft3vZm=w400-h400-l90-rj',
+                }}
+                style={StyleSheet.absoluteFillObject}
+                blurRadius={10}
+            /> */}
+
             <DoubleTap
                 onDoubleTap={() => {
                     console.log('TAPPED')
                 }}>
-                <ImageBackground
+                <ProgressSlider />
+
+                {/* <ImageBackground
                     source={{
                         uri:
                             current.artwork ||
-                            'https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg',
+                            'https://lh3.googleusercontent.com/fx6kz_AXXbWVglzvmChcvFRTwV_nFmSAjuyYUO2XTqO_Es9hv4Fe6scz1pTfNWw6hSWob6gtpsft3vZm=w400-h400-l90-rj',
                     }}
                     resizeMode="cover"
                     style={globalStyles.fullImageBackground}
                     blurRadius={MUSIC_PLAYER_BLUR + 30}>
-                    <ProgressSlider />
-                </ImageBackground>
+                </ImageBackground> */}
             </DoubleTap>
         </GradientBackground>
     )
