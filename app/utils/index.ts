8b064d3c8bd7ed04a11b83f51e10ea50f5486fa4
sobhanbera <sobhanbera258@gmail.com>
2021-07-15 19:14:31 +0530

    more utils on place

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -6,5 +6,33 @@ import {
 } from './Colors'
 export {isColorLight, isColorDark, colorBrightness, sortColorsBasedOnBrightness}
 
-import {returnNullCallback, IF} from './Objects'
-export {returnNullCallback, IF}
+import {
+    returnNullCallback,
+    IF,
+    IMAGE_WIDTH_HEIGHT_FORMATTING_REGEX,
+    firstLetterCap,
+    formatArtistSingle,
+    formatArtists,
+    formatArtistsListFromArray,
+    getHighQualityImage,
+    getHighQualityImageFromSong,
+    getHighQualityImageFromSongImage,
+    getHighQualityImageFromSongImageArray,
+    getHightQualityImageFromLink,
+    trimLargeString,
+} from './Objects'
+export {
+    returnNullCallback,
+    IF,
+    IMAGE_WIDTH_HEIGHT_FORMATTING_REGEX,
+    firstLetterCap,
+    formatArtistSingle,
+    formatArtists,
+    formatArtistsListFromArray,
+    getHighQualityImage,
+    getHighQualityImageFromSong,
+    getHighQualityImageFromSongImage,
+    getHighQualityImageFromSongImageArray,
+    getHightQualityImageFromLink,
+    trimLargeString,
+}
