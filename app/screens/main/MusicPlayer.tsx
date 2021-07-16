00ff51065e89ced43fc7927d132ac0fbcf500a2c
sobhanbera <sobhanbera258@gmail.com>
2021-07-16 23:54:00 +0000

    image carousel animation

--- app/screens/main/MusicPlayer.tsx
+++ app/screens/main/MusicPlayer.tsx
@@ -1,33 +1,39 @@
 import React, {useEffect, useState} from 'react'
-import {View, Alert, Dimensions} from 'react-native'
-
+import {
+    View,
+    Image,
+    Dimensions,
+    FlatList,
+    StyleSheet,
+    StatusBar,
+    Animated,
+} from 'react-native'
 import ImageColors from 'react-native-image-colors'
-import {Image} from 'react-native'
-import {StyleSheet} from 'react-native'
-import globalStyles from '../../styles/global.styles'
+
 import {LINEAR_GRADIENT_LOCATIONS_4} from '../../constants'
 import {DoubleTap, GradientBackground, ProgressSlider} from '../../components'
-import {useMusicApi, usePlayer, usePrompt, useTheme} from '../../context'
+import {usePlayer, usePrompt, useTheme} from '../../context'
 import {DominatingColors} from '../../interfaces'
 import {
     sortColorsBasedOnBrightness,
     getHightQualityImageFromLink,
 } from '../../utils'
-import {ImageBackground} from 'react-native'
-import {parseNextPanel, parseSongSearchResult} from '../../api/parsers'
 
-// const {width, height} = Dimensions.get('window')
-const IMAGE_WIDTH = 210
-const IMAGE_HEIGHT = IMAGE_WIDTH // same as the WIDTH of image
-const IMAGE_BORDER_RADIUS = 12
+const {width} = Dimensions.get('screen')
+const IMAGE_WIDTH = width * 0.7
+const IMAGE_HEIGHT = IMAGE_WIDTH * 1.2 // same as the WIDTH of image
+const IMAGE_BORDER_RADIUS = 10
+const IMAGE_BLUR_RADIUS = 50
 
 interface PlayerProps {
     navigation?: any
 }
 const Player: React.FC<PlayerProps> = props => {
-    const {current} = usePlayer()
+    const {current, nextSongsList} = usePlayer()
     const {prompt} = usePrompt()
     const {themeColors} = useTheme()
+
+    const scrollX = React.useRef(new Animated.Value(0)).current
 
     const [colors, setColors] = useState<string[]>([
         themeColors.rgbstreakgradient[1],
@@ -35,6 +41,8 @@ const Player: React.FC<PlayerProps> = props => {
         themeColors.rgbstreakgradient[3],
         themeColors.rgbstreakgradient[5],
     ])
+
+    // useEffect(() => {}, [nextSongsList[0].id])
 
     useEffect(() => {
         if (current.artwork)
@@ -83,20 +91,86 @@ const Player: React.FC<PlayerProps> = props => {
                 justifyContent: 'center',
                 alignItems: 'center',
             }}>
-            <ImageBackground
-                source={{
-                    uri: current.artwork,
+            <View style={StyleSheet.absoluteFillObject}>
+                {nextSongsList.map((song, _) => {
+                    const inputRange = [
+                        (_ - 1) * width,
+                        _ * width,
+                        (_ + 1) * width,
+                    ]
+                    const opacity = scrollX.interpolate({
+                        inputRange: inputRange,
+                        outputRange: [0, 1, 0],
+                    })
+                    return (
+                        <Animated.Image
+                            key={`${song.id}-${_}`}
+                            style={[
+                                StyleSheet.absoluteFillObject,
+                                {
+                                    opacity: opacity,
+                                },
+                            ]}
+                            source={{uri: song.artwork}}
+                            blurRadius={IMAGE_BLUR_RADIUS}
+                        />
+                    )
+                })}
+            </View>
+
+            <Animated.FlatList
+                onScroll={Animated.event(
+                    [{nativeEvent: {contentOffset: {x: scrollX}}}],
+                    {useNativeDriver: true},
+                )}
+                horizontal
+                pagingEnabled
+                bounces={true}
+                showsHorizontalScrollIndicator={false}
+                data={nextSongsList}
+                keyExtractor={(item, _) => `${item.id}-${_}`}
+                renderItem={({item}) => {
+                    return (
+                        <View
+                            style={{
+                                width: width,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}>
+                            <View
+                                style={{
+                                    width: IMAGE_WIDTH,
+                                    height: IMAGE_HEIGHT,
+                                    borderRadius: IMAGE_BORDER_RADIUS,
+                                    overflow: 'hidden',
+                                    elevation: 10,
+                                }}>
+                                <Image
+                                    fadeDuration={1000}
+                                    source={{uri: item.artwork}}
+                                    style={{
+                                        width: IMAGE_WIDTH,
+                                        height: IMAGE_HEIGHT,
+                                        borderRadius: IMAGE_BORDER_RADIUS,
+                                        overflow: 'hidden',
+                                        resizeMode: 'cover',
+                                    }}
+                                />
+                            </View>
+                        </View>
+                    )
                 }}
-                resizeMode="cover"
-                style={{
-                    width: '100%',
-                    height: '75%',
-                    // opacity: 0.5,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    elevation: 0,
-                }}
-                blurRadius={0}></ImageBackground>
+            />
+
+            <Image
+                source={{uri: current.artwork}}
+                style={[
+                    StyleSheet.absoluteFillObject,
+                    {
+                        opacity: 0,
+                    },
+                ]}
+            />
         </GradientBackground>
     )
 }
