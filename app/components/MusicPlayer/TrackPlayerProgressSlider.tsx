915576122e47347718a0fd4775084288b0688949
sobhanbera <sobhanbera258@gmail.com>
2021-07-16 21:39:31 +0530

    updated progress bar component

--- app/components/MusicPlayer/TrackPlayerProgressSlider.tsx
+++ app/components/MusicPlayer/TrackPlayerProgressSlider.tsx
@@ -2,9 +2,13 @@ import React, {useEffect} from 'react'
 import {View, Button} from 'react-native'
 import Slider from '@react-native-community/slider'
 
-import {useFetcher, usePlayer, usePlayerProgress} from '../../context'
+import {usePlayer, usePlayerProgress} from '../../context'
 
-const TrackPlayerProgressSlider = () => {
+interface Props {
+    duration: number
+    index?: number
+}
+const TrackPlayerProgressSlider = (props: Props) => {
     const {position, duration, bufferedPosition} = usePlayerProgress()
     const {playing, paused, buffering, seekTo, seekInterval, playonly, pause} =
         usePlayer()
@@ -14,22 +18,29 @@ const TrackPlayerProgressSlider = () => {
             style={{
                 justifyContent: 'center',
                 alignItems: 'center',
+                width: '100%',
             }}>
-            <Slider
-                value={position}
-                minimumValue={0}
-                maximumValue={duration}
-                maximumTrackTintColor="black"
-                thumbTintColor="black"
-                minimumTrackTintColor="blue"
-                onSlidingComplete={e => {
-                    seekTo(e)
-                }}
+            <View
                 style={{
                     width: '90%',
                     maxWidth: 330,
-                }}
-            />
+                }}>
+                <Slider
+                    value={position} // position is in seconds
+                    minimumValue={0}
+                    maximumValue={props.duration || duration / 1000} // duration is in milliseconds, but props.duration must be in seconds
+                    maximumTrackTintColor="black"
+                    thumbTintColor="black"
+                    minimumTrackTintColor="blue"
+                    onSlidingComplete={e => {
+                        seekTo(e / 1000) // the e is in milliseconds
+                        // console.log(e)
+                    }}
+                    style={{
+                        width: '100%',
+                    }}
+                />
+            </View>
         </View>
     )
 }
