21d2ab90a350795dce8cde4feb304c9d7365b19a
sobhanbera <sobhanbera258@gmail.com>
2021-07-24 01:10:09 +0530

    main infinite scroll implementation is remaining

--- app/screens/main/Explore/SongCategory.tsx
+++ app/screens/main/Explore/SongCategory.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react'
+import {Dimensions, ScrollView, ActivityIndicator} from 'react-native'
 
 import {AnimatedHeader} from '../../../components'
 import {
@@ -8,6 +9,9 @@ import {
 } from '../../../interfaces'
 import {useMusicApi} from '../../../api'
 import CommonSongList from '../../../components/CommonSongList'
+import {useTheme} from '../../../context'
+
+const {width} = Dimensions.get('window')
 
 interface Props {
     navigation?: any
@@ -20,33 +24,77 @@ interface Props {
 }
 const SongCategoryScreen = (props: Props) => {
     const {category, headerTitle} = props.route.params
-    const {search} = useMusicApi()
+    const {search, getContinuation} = useMusicApi()
+    const {themeColors} = useTheme()
+
+    const [loading, setLoading] = useState<boolean>(false)
 
     const [songs, setSongs] = useState<FetchedSongObject>(
         BareFetchedSongObjectInstance,
     )
 
+    let i = 0
+    const continueLoadingData = () => {
+        console.log('MORE DATA LOADING', i++)
+        if (
+            loading === false &&
+            songs.continuation.clickTrackingParams &&
+            songs.continuation.continuation
+        ) {
+            console.log('REACHED', i++)
+            setLoading(true)
+            getContinuation('search', songs.continuation)
+                .then((res: FetchedSongObject) => {
+                    const data = songs.content.concat(res.content)
+                    setSongs({
+                        content: data,
+                        continuation: res.continuation,
+                    })
+                    setLoading(false)
+                })
+                .catch(_err => {
+                    setLoading(false)
+                })
+        }
+    }
+
     useEffect(() => {
         search(category.name, 'SONG')
             .then((res: FetchedSongObject) => {
-                console.log(res.content.length)
                 setSongs(res)
             })
             .catch(_err => {})
     }, [category.id])
 
-    let i = 0
-
     return (
         <AnimatedHeader
             infiniteScrollOffset={550} // 550 px from the down of the screen when the onreachend function will be triggered
-            onReachedEnd={() => {
-                console.log(i++)
-            }}
+            onReachedEnd={continueLoadingData}
             headerImage={category.highimage}
             headerNameTitle={category.name}
             headerTitle={headerTitle || category.name}>
-            <CommonSongList songs={songs.content} />
+            <ScrollView
+                horizontal
+                pagingEnabled
+                bounces={true}
+                snapToAlignment={'end'}
+                scrollEventThrottle={16}
+                snapToInterval={width}
+                showsHorizontalScrollIndicator={false}
+                showsVerticalScrollIndicator={false}>
+                {/* songs list */}
+                <CommonSongList songs={songs.content} />
+
+                {/* artists list */}
+                <CommonSongList songs={songs.content} />
+
+                {loading ? (
+                    <ActivityIndicator
+                        color={themeColors.white[0]}
+                        size={'small'}
+                    />
+                ) : null}
+            </ScrollView>
         </AnimatedHeader>
     )
 }
