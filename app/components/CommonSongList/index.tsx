f1baf60fcb8e8b3d1830ab9e5ecd300a10a921f2
sobhanbera <sobhanbera258@gmail.com>
2021-07-23 23:56:44 +0530

    new component for the list of common songs

---
+++ app/components/CommonSongList/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+import {SongObject} from '../../interfaces'
+
+interface Props {
+    songs: Array<SongObject>
+}
+const CommonSongList = (props: Props) => {
+    return <View></View>
+}
+
+export default CommonSongList
