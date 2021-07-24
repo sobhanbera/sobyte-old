2e5942f050169b8284c5209d3ba96155145d39cd
sobhanbera <sobhanbera258@gmail.com>
2021-07-24 19:04:06 +0530

    song category design is fully changed

--- app/screens/main/Explore/SongCategory.tsx
+++ app/screens/main/Explore/SongCategory.tsx
@@ -1,13 +1,6 @@
 import React, {useEffect, useState} from 'react'
-import {
-    Dimensions,
-    ScrollView,
-    ActivityIndicator,
-    View,
-    Text,
-} from 'react-native'
+import {ActivityIndicator} from 'react-native'
 
-import {AnimatedHeader} from '../../../components'
 import {
     BareFetchedArtistObjectInstance,
     BareFetchedSongObjectInstance,
@@ -16,38 +9,14 @@ import {
     SongCategory,
 } from '../../../interfaces'
 import {useMusicApi} from '../../../api'
-import {CommonSongList, CommonArtistList} from '../../../components'
 import {useTheme} from '../../../context'
-import {songComponentsStyles} from '../../../styles/global.styles'
+import {
+    AnimatedHeader,
+    CommonSongList,
+    GridArtistList,
+    TopicTitle,
+} from '../../../components'
 
-const {width} = Dimensions.get('screen')
-
-const DataList = [
-    {
-        name: 'Songs',
-        id: 'songs',
-        selected: 0,
-    },
-    {
-        name: 'Artists',
-        id: 'artists',
-        selected: 1,
-    },
-]
-interface OnScrollProps {
-    layoutMeasurement: {
-        height: number
-        width: number
-    }
-    contentOffset: {
-        x: number
-        y: number
-    }
-    contentSize: {
-        height: number
-        width: number
-    }
-}
 interface Props {
     navigation?: any
     route: {
@@ -68,7 +37,6 @@ const SongCategoryScreen = (props: Props) => {
     // this variable will control the flow of loading data
     const [loading, setLoading] = useState<boolean>(false)
     // and this constant will control what type of data to show in the particular UI
-    const [selected, setSelected] = useState<any>(DataList[0].selected)
 
     /**
      * songs list, artists list, playlists data, albums,
@@ -80,10 +48,6 @@ const SongCategoryScreen = (props: Props) => {
     const [artists, setArtists] = useState<FetchedArtistObject>(
         BareFetchedArtistObjectInstance,
     )
-
-    // scrollview reference variable to perform button pressed and auto scroll animation
-    // and many more...
-    const contentScroll = React.useRef<ScrollView>(null)
 
     const continueLoadingData = () => {
         // this function will be called each time when the user is getting closed to the
@@ -136,40 +100,8 @@ const SongCategoryScreen = (props: Props) => {
             setSongs(BareFetchedSongObjectInstance)
             setArtists(BareFetchedArtistObjectInstance)
             setLoading(false)
-            setSelected(DataList[0].selected)
         }
     }, [category.id])
-
-    const onScroll = (scrollProps: OnScrollProps) => {
-        // this is the current position of the scroll view on the UI
-        const currentRoughEstimatedScrollPosition = Math.floor(
-            scrollProps.contentOffset.x,
-        )
-        // we are checking if the scroll view is around any offset of any type of content
-        // in this way we are not setting state many time
-        // if the difference is around 5px we will set the state
-        if (currentRoughEstimatedScrollPosition % Math.floor(width) <= 5) {
-            const currSelected = Math.floor(
-                currentRoughEstimatedScrollPosition / Math.floor(width),
-            )
-            setSelected(currSelected)
-        }
-    }
-
-    const changeData = (selector: any) => {
-        // when the user clicks on any data type button
-        // scrollview will scroll to that position
-        // for ex: for songs it will move to `width*0 = 0` position
-        // and for artists it will move to `width*1 = width` position
-        // and playlist it will move to `width*2` position
-        // and so on.
-        setSelected(selector)
-        contentScroll.current?.scrollTo({
-            animated: true,
-            x: width * selector,
-            y: 0,
-        })
-    }
 
     return (
         <AnimatedHeader
@@ -178,51 +110,20 @@ const SongCategoryScreen = (props: Props) => {
             headerImage={category.highimage}
             headerNameTitle={category.name}
             headerTitle={headerTitle || category.name}>
-            {/* different topics or tags */}
-            <ScrollView
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                showsVerticalScrollIndicator={false}>
-                <View
-                    style={
-                        songComponentsStyles.containerTopicTextHolderSection
-                    }>
-                    {DataList.map(data => {
-                        return (
-                            <Text
-                                key={data.id}
-                                onPress={() => changeData(data.selected)}
-                                style={[
-                                    songComponentsStyles.containerTopicText,
-                                    selected == data.selected
-                                        ? songComponentsStyles.containerTopicTextSelected
-                                        : {},
-                                ]}>
-                                {data.name}
-                            </Text>
-                        )
-                    })}
-                </View>
-            </ScrollView>
-
             {/* main content here */}
-            <ScrollView
-                ref={contentScroll}
-                onScroll={({nativeEvent}) => onScroll(nativeEvent)}
-                horizontal
-                pagingEnabled
-                bounces={true}
-                snapToAlignment={'center'}
-                scrollEventThrottle={16}
-                snapToInterval={width}
-                showsHorizontalScrollIndicator={false}
-                showsVerticalScrollIndicator={false}>
-                {/* songs list */}
-                <CommonSongList songs={songs.content} />
+            <TopicTitle title={'Artists'} />
+            <GridArtistList
+                content={artists.content}
+                contentLength={artists.content.length}
+                subColor={themeColors.text[0] + '70'}
+                textColor={themeColors.text[0] + 'E7'}
+                shimmerDirection={'up'}
+                scrollDirection="horizontal"
+                id={'demo'}
+            />
 
-                {/* artists list */}
-                <CommonArtistList artists={artists.content} />
-            </ScrollView>
+            <TopicTitle title={'Songs'} />
+            <CommonSongList songs={songs.content} />
 
             {loading ? (
                 <ActivityIndicator
