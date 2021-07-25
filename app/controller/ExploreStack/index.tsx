f4cd9437688143dc354f324bba51567d5c3b296b
sobhanbera <sobhanbera258@gmail.com>
2021-07-25 23:00:57 +0530

    extra songs screen added to main navigation

--- app/controller/ExploreStack/index.tsx
+++ app/controller/ExploreStack/index.tsx
@@ -5,8 +5,9 @@ import Entypo from 'react-native-vector-icons/Entypo'
 import Explore from '../../screens/main/Explore/Explore'
 import SearchResult from '../../screens/main/Explore/SearchResult'
 import SongCategory from '../../screens/main/Explore/SongCategory'
-import {ARTIST_DETAILS_SCREEN} from '../../constants'
+import {ARTIST_DETAILS_SCREEN, EXTRA_SONGS_SCREEN} from '../../constants'
 import ArtistDetail from '../../screens/main/common/ArtistDetail'
+import ExtraSongs from '../../screens/main/common/ExtraSongs'
 
 const ProfileStack = createStackNavigator()
 interface Props {}
@@ -43,6 +44,25 @@ const ProfileStackNavigator = (props: Props) => {
             <ProfileStack.Screen
                 name={ARTIST_DETAILS_SCREEN}
                 component={ArtistDetail}
+                options={{
+                    headerShown: true,
+                    headerTitle: '',
+                    headerTransparent: true,
+                    headerTintColor: '#FFFFFF',
+                    headerBackImage: () => (
+                        <Entypo
+                            name="chevron-thin-left"
+                            color={'white'}
+                            size={20}
+                        />
+                    ),
+                }}
+            />
+
+            {/* this screen must also be the same constant or many parts of the app may break when launching this screen */}
+            <ProfileStack.Screen
+                name={EXTRA_SONGS_SCREEN}
+                component={ExtraSongs}
                 options={{
                     headerShown: true,
                     headerTitle: '',
