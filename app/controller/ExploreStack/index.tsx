2f28c367d501882cd570730436d5df0c7cfd1701
sobhanbera <sobhanbera258@gmail.com>
2021-07-25 21:19:16 +0530

    added artist details screen to the explorer screens main navigation

--- app/controller/ExploreStack/index.tsx
+++ app/controller/ExploreStack/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import {createStackNavigator} from '@react-navigation/stack'
+import Entypo from 'react-native-vector-icons/Entypo'
 
 import Explore from '../../screens/main/Explore/Explore'
 import SearchResult from '../../screens/main/Explore/SearchResult'
 import SongCategory from '../../screens/main/Explore/SongCategory'
-import Entypo from 'react-native-vector-icons/Entypo'
+import { ARTIST_DETAILS_SCREEN } from '../../constants'
+import ArtistDetail from '../../screens/main/common/ArtistDetail'
 
 const ProfileStack = createStackNavigator()
 interface Props {}
 const ProfileStackNavigator = (props: Props) => {
+    console.log("TEST", props)
     return (
         <ProfileStack.Navigator
             screenOptions={{
@@ -37,6 +40,8 @@ const ProfileStackNavigator = (props: Props) => {
                     ),
                 }}
             />
+            {/* this screen must be the same constant or many parts of the app may break when launching this screen */}
+            <ProfileStack.Screen name={ARTIST_DETAILS_SCREEN} component={ArtistDetail} />
         </ProfileStack.Navigator>
     )
 }
