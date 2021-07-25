1c515b30b9a40c84a2cb798a2fd95373a3d31116
sobhanbera <sobhanbera258@gmail.com>
2021-07-25 18:36:38 +0530

    debug log added waiting for next trigger

--- app/components/GridArtistList/GridArtistItem.tsx
+++ app/components/GridArtistList/GridArtistItem.tsx
@@ -19,16 +19,15 @@ interface Props {
 
 const GridArtistItem = React.memo(
     ({item, index, subColor, textColor}: Props) => {
-        const highQualityImage = useMemo(
-            () =>
-                getHightQualityImageFromLinkWithHeight(
+        if(!item.thumbnails[0].url) {
+            console.log("THUMBNAIL NOT FOUND GRID ARTIST ITEM", item)
+        }
+        const highQualityImage = getHightQualityImageFromLinkWithHeight(
                     item.thumbnails[0].url,
                     item.thumbnails[0].height,
                     720,
                     100,
-                ),
-            [],
-        )
+                )
 
         return (
             <Pressable onPress={() => {}}>
