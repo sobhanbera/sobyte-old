514c7df392f8196306eca8310df1a0874c535dc2
sobhanbera <sobhanbera258@gmail.com>
2021-07-25 22:09:28 +0530

    added more accurate sorting of colors and one function to convert hex to hsb color format

--- app/utils/Colors.ts
+++ app/utils/Colors.ts
@@ -20,6 +20,39 @@ export function colorBrightness(color: any = '') {
     return brightness
 }
 
+/**
+ * function which converts Hex value of color to 
+ * HSB (hue, saturation, and brightness) format...
+*/
+export function hexToHSB(hex: string = ''): number[] {
+    hex = hex.replace(/^#/, '');
+    hex = hex.length === 3 ? hex.replace(/(.)/g, '$1$1') : hex;
+
+    var red = parseInt(hex.substr(0, 2), 16) / 255,
+      green = parseInt(hex.substr(2, 2), 16) / 255,
+      blue = parseInt(hex.substr(4, 2), 16) / 255;
+
+    var cMax = Math.max(red, green, blue),
+      cMin = Math.min(red, green, blue),
+      delta = cMax - cMin,
+      saturation = cMax ? (delta / cMax) : 0;
+
+    switch (cMax) {
+        case 0:
+            return [0, 0, 0];
+        case cMin:
+            return [0, 0, cMax];
+        case red:
+            return [60 * (((green - blue) / delta) % 6) || 0, saturation, cMax];
+        case green:
+            return [60 * (((blue - red) / delta) + 2) || 0, saturation, cMax];
+        case blue:
+            return [60 * (((red - green) / delta) + 4) || 0, saturation, cMax];
+        default:
+            return [0, 0, 0,]
+    }
+  }
+
 export type ColorsSortingDirection = 'light-first' | 'dark-first' | 'default'
 export function sortColorsBasedOnBrightness(
     colors: Array<any>,
@@ -33,6 +66,24 @@ export function sortColorsBasedOnBrightness(
         return colors
             .sort((a, b) => {
                 return colorBrightness(a) - colorBrightness(b)
+            })
+            .reverse()
+}
+
+export function sortColors(
+    colors: Array<string>,
+    direction: ColorsSortingDirection = 'default',
+) {
+    if (direction === 'light-first')
+        return colors.sort((a, b) => {
+            const color1 = hexToHSB(a)[0]
+            const color2 = hexToHSB(b)[0]
+            return color1 - color2
+        })
+    else
+        return colors
+            .sort((a, b) => {
+                return hexToHSB(a)[0] - hexToHSB(b)[0]
             })
             .reverse()
 }
