9e17a1c369b932cefbd96ae4dbba7a7662a21991
sobhanbera <sobhanbera258@gmail.com>
2021-07-26 17:02:02 +0530

    activity updates for splash screen

--- android/app/src/main/java/com/sobyte/SplashActivity.java
+++ android/app/src/main/java/com/sobyte/SplashActivity.java
@@ -1,2 +1,19 @@
-package com.sobyte;public class SplashActivity {
+package com.sobyte;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Application;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactApplication;
+
+public class SplashActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
