446b9d4d97764a68caa0ab6eaff58f5c4f3c5893
sobhanbera <sobhanbera258@gmail.com>
2021-07-28 14:17:21 +0530

    new design for explore tab header collapsible component

--- app/components/HeaderCollapsible/index.tsx
+++ app/components/HeaderCollapsible/index.tsx
@@ -5,16 +5,18 @@ import {useTranslation} from 'react-i18next'
 import {HEADER_MIN_HEIGHT} from '../../constants'
 import globalStyles from '../../styles/global.styles'
 import {useTheme} from '../../context'
+import { TextInput } from 'react-native'
 
 interface Props {
     headerScrollHeight?: any
     headerScrollColor: any
     right?: React.ReactNode
     onPress: Function
+    onInputFocus: Function
 }
 const HeaderCollapsible: React.FC<Props> = props => {
     const {t} = useTranslation()
-    const {rgbstreakgradient} = useTheme().themeColors
+    const {rgbstreakgradient, placeholder} = useTheme().themeColors
     /**
      * text color changing animation variables and functions
      */
@@ -63,22 +65,21 @@ const HeaderCollapsible: React.FC<Props> = props => {
     }
 
     return (
-        <View
-            style={{
+        <View style={{
+            flexDirection: 'column',
+            paddingVertical: 10,
+            backgroundColor: props.headerScrollColor,
+        }}>
+            <View style={{
                 height: props.headerScrollHeight || HEADER_MIN_HEIGHT,
                 width: '100%',
                 backgroundColor: props.headerScrollColor,
-                paddingHorizontal: 12,
+                justifyContent: 'flex-start',
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginHorizontal: 20,
+                paddingHorizontal: 10,
             }}>
-            <View
-                style={{
-                    paddingHorizontal: 12,
-                    paddingVertical: 10,
-                    flex: 1,
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    justifyContent: 'space-between',
-                }}>
                 <Pressable
                     onPress={() => {
                         handleLogoRotateAnimation()
@@ -103,9 +104,90 @@ const HeaderCollapsible: React.FC<Props> = props => {
                     ]}>
                     {t('common:appName')}
                 </Animated.Text>
-                {props.right}
+            </View>
+            <View>
+            <TextInput 
+                style={{
+                    backgroundColor: props.headerScrollColor,
+                    marginHorizontal: 20,
+                    paddingHorizontal: 10,
+                    paddingVertical: 8,
+                    borderWidth: 0.5,
+                    borderColor: placeholder[0],
+                    borderRadius: 100,
+                    fontSize: 16
+                }} 
+                value=""
+                placeholder="Search Songs, Artists...."
+                placeholderTextColor={placeholder[0]}
+                onFocus={() => props.onInputFocus()}
+            />
             </View>
         </View>
+        // <View style={{
+        //         backgroundColor: props.headerScrollColor,
+        //     }}
+        // >
+        //     <View
+        //         style={{
+        //             height: props.headerScrollHeight || HEADER_MIN_HEIGHT,
+        //             width: '100%',
+        //             backgroundColor: props.headerScrollColor,
+        //             paddingHorizontal: 12,
+        //         }}>
+        //         <View
+        //             style={{
+        //                 paddingHorizontal: 12,
+        //                 paddingVertical: 10,
+        //                 flex: 1,
+        //                 flexDirection: 'row',
+        //                 alignItems: 'center',
+        //                 justifyContent: 'space-between',
+        //             }}>
+        //             <Pressable
+        //                 onPress={() => {
+        //                     handleLogoRotateAnimation()
+        //                     props.onPress()
+        //                 }}>
+        //                 <Animated.Image
+        //                     source={require('../../assets/images/sobyte_logo_white.png')}
+        //                     style={{
+        //                         height: 27.6,
+        //                         width: 21.8,
+        //                         transform: [{rotateZ: rotate}],
+        //                     }}
+        //                 />
+        //             </Pressable>
+        //             <Animated.Text
+        //                 style={[
+        //                     globalStyles.appName,
+        //                     {
+        //                         color: color,
+        //                         fontSize: 25,
+        //                     },
+        //                 ]}>
+        //                 {t('common:appName')}
+        //             </Animated.Text>
+        //             {props.right}
+        //         </View>
+        //     </View>
+        //     <TextInput 
+        //         style={{
+        //             backgroundColor: props.headerScrollColor,
+        //             paddingHorizontal: 10,
+        //             paddingVertical: 8,
+        //             marginHorizontal: 20,
+        //             borderWidth: 0.5,
+        //             borderColor: placeholder[0],
+        //             borderRadius: 100,
+        //             fontSize: 16
+        //         }} 
+        //         value=""
+        //         placeholder="Search Songs, Artists...."
+        //         placeholderTextColor={placeholder[0]}
+        //         onFocus={() => props.onInputFocus()}
+        //     />
+        // </View>
     )
 }
 
