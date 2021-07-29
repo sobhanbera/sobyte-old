43cf76fb8e65ed6a48409cd6252112ededb7a7a5
sobhanbera <sobhanbera258@gmail.com>
2021-07-29 21:55:29 +0530

    new constant file which will contain all the limits constants

---
+++ app/constants/limits.js
@@ -0,0 +1,8 @@
+/**
+ * this file contains all those limits constant like
+ * max length, min length and such things...
+ */
+
+export const MAX_EMAIL_LENGTH = 320
+export const MAX_USERNAME_LENGTH = 30
+export const MAX_PASSWORD_LENGTH = 30
