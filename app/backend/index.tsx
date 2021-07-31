0a12458e38bf231823e1ca6e71c55956fab4d844
sobhanbera <sobhanbera258@gmail.com>
2021-07-31 22:52:58 +0530

    main api back bone created

--- app/backend/index.tsx
+++ app/backend/index.tsx
@@ -1,11 +1,58 @@
 import React from 'react'
+import axios, {AxiosRequestConfig} from 'axios'
 
-const AppMainBackendApiContext = React.createContext({})
+import MAIN_API_ENDPOINT from '../../app/constants/endPoints'
+import {APP_BACKEND_API_KEY} from '../../app/constants'
+
+type ApiRequestType =
+    | 'all'
+    | 'get'
+    | 'post'
+    | 'put'
+    | 'delete'
+    | 'patch'
+    | 'options'
+    | 'head'
+const AppMainBackendApiContext = React.createContext({
+    makeApiRequestPOST: (
+        _endpoint: string,
+        _dataToPost: any,
+        _apiConfigs: AxiosRequestConfig,
+    ) => {},
+})
 interface Props {
     children: React.ReactNode
 }
 const AppMainBackendApi = (props: Props) => {
-    const appMainBackendApiContextValues = {}
+    const defaultApiBody = {
+        key: APP_BACKEND_API_KEY,
+        ip: '',
+    }
+    let backendApiClient = axios.create({
+        baseURL: MAIN_API_ENDPOINT,
+        headers: {
+            'content-type': 'application/json',
+        },
+        withCredentials: true,
+    })
+
+    function makeApiRequestPOST(
+        endpoint: string,
+        dataToPost: any,
+        _apiConfigs: AxiosRequestConfig,
+    ) {
+        backendApiClient.post(
+            endpoint,
+            {
+                ...dataToPost,
+            },
+            _apiConfigs,
+        )
+    }
+
+    const appMainBackendApiContextValues = {
+        makeApiRequestPOST: makeApiRequestPOST,
+    }
     return (
         <AppMainBackendApiContext.Provider
             value={appMainBackendApiContextValues}>
