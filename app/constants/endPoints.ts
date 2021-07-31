2b435c5676c488eacd0a60effebb8e3020ab86ee
sobhanbera <sobhanbera258@gmail.com>
2021-07-31 20:49:54 +0530

    constant for end points is added

---
+++ app/constants/endPoints.ts
@@ -0,0 +1,9 @@
+// main api endpoint url...
+const MAIN_API_ENDPOINT = 'http://localhost:8000'
+export default MAIN_API_ENDPOINT
+
+// other sub routes of the api endpoint are...
+export const SIGN_IN_ROUTE_ENDPOINT = '/authentication/signin'
+export const SIGN_UP_ROUTE_ENDPOINT = '/authentication/signup'
+export const UPDATE_RRRLAST_LOGIN_ROUTE_ENDPOINT =
+    '/authentication/updatelastlogin'
