36b10e554a3eb15d11c4c0198357b276f4d7d14d
sobhanbera <sobhanbera258@gmail.com>
2021-07-31 22:33:32 +0530

    context updates

--- app/context/index.ts
+++ app/context/index.ts
@@ -1,11 +1,16 @@
-import {useMusicApi} from '../api'
-import {useApp} from '../MainApp'
-import {useTheme} from '../themes/ThemeProvider'
-import {useFetcher} from '../api/MusicFetcher'
-import {usePrompt} from '../components/Prompt'
-import Setting, {useSetting} from './Settings'
+// the main app backend api context provider
+import {useBackendApi, AppMainBackendApiContext} from '../backend' // the major and main secure backend api of the application
+export {useBackendApi, AppMainBackendApiContext}
 
-import Player, {usePlayer} from '../api/PlayerControls'
+// other context providers
+import {useMusicApi} from '../api' // the main music loader
+import {useApp} from '../MainApp' // the app starting
+import {useTheme} from '../themes/ThemeProvider' // theme context
+import {useFetcher} from '../api/MusicFetcher' // track featching methods
+import {usePrompt} from '../components/Prompt' // global app prompts
+import Setting, {useSetting} from './Settings' // global settings
+
+import Player, {usePlayer} from '../api/PlayerControls' // global player options
 
 export default Player
 export {Setting}
