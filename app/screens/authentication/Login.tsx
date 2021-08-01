8758bffee0861950a42042b16922310410b4c6f3
sobhanbera <sobhanbera258@gmail.com>
2021-08-01 23:26:35 +0530

    LOGIN FEATURE IMPLEMENTED fixed #22

--- app/screens/authentication/Login.tsx
+++ app/screens/authentication/Login.tsx
@@ -11,27 +11,120 @@ import {
     PaddingView,
     SobyteTextInput,
     ScalerAuthButton,
-    SobyteAlertBox,
 } from '../../components'
-
-import {useTheme, useApp, useUserData, useBackendApi} from '../../context'
+import {
+    useTheme,
+    useApp,
+    usePrompt,
+    useUserData,
+    useBackendApi,
+} from '../../context'
+import {
+    MAX_EMAIL_LENGTH,
+    MAX_PASSWORD_LENGTH,
+    MIN_PASSWORD_LENGTH,
+} from '../../constants/limits'
+import {EMAIL_REGEX} from '../../constants'
+import {isEmailBlocked} from '../../utils'
+import {SIGN_IN_ROUTE_ENDPOINT} from '../../constants/endPoints'
 
 interface LoginProps {
     navigation: any
 }
 const Login = (props: LoginProps) => {
-    const {themeColors} = useTheme()
-    const {setShowLoading} = useApp()
-    const {} = useUserData()
-    const {} = useBackendApi()
+    const {themeColors} = useTheme() // theme context api provider...
+    const {setShowLoading} = useApp() // loader context api
+    const {prompt} = usePrompt() // prompt context api
+    const {setLocalUserData} = useUserData() // user data, load user data and many more
+    const {makeApiRequestPOST} = useBackendApi() // to make API requests to the backend...
 
-    const [visible, setVisibility] = useState(false)
+    const [user, setUser] = useState('sobhanbera258@gmail.com') // the username or email id field text...
+    const [password, setPassword] = useState('sobhanber') // the password field text...
 
-    const [email, setEmail] = useState('')
-
+    /**
+     * this is the main login function of the application
+     * the checks are as follows...
+     * 1. email and password should be present for regular signin option (not by OTP)
+     * 2. email should be valid
+     * 3. email should not be a temporary mail ID
+     * 4. password length should be valid
+     * 5. if the user provided an email checking if it is of correct length
+     * if all the above checks are passed we could login the user with a api request...
+     * some above checks are temporary changed to this
+     * 2,3. check if the user text is a type of email the if it is a email check wheather it is a temporary mail if it is then don't continue else continue...
+     */
     const LoginUser = () => {
-        console.log('USER REGISTRATION STARTED...')
-        setVisibility(true)
+        if (!user) {
+            //  * 1. email should be present for regular signin option (not by OTP)
+            prompt('Email is required for signing in.', 'warning')
+        } else if (EMAIL_REGEX.test(user) && isEmailBlocked(user)) {
+            // 2. email should be valid
+            // 3. email should not be a temporary mail ID
+            // 2,3. check if the user text is a type of email the if it is a email check wheather it is a temporary mail if it is then don't continue else continue...
+            prompt('Account will be blocked if you use this email.', 'warning')
+            // prompt('Enter a valid email ID.', 'warning')
+        } else if (EMAIL_REGEX.test(user) && user.length > MAX_EMAIL_LENGTH) {
+            // 5. if the user provided an email checking if it is of correct length
+            prompt('Valid email length must be under 320.', 'warning')
+        } else if (!password) {
+            // 1. password should be present for regular signin option (not by OTP)
+            prompt('Passoword is required for regular signing in.', 'warning')
+        } else if (password.length < MIN_PASSWORD_LENGTH) {
+            // 4. password length should be valid
+            prompt(
+                'SHORT: Password length must be of 8-30 and alphanumeric.',
+                'warning',
+            )
+        } else if (password.length > MAX_PASSWORD_LENGTH) {
+            // 4. password length should be valid
+            prompt(
+                'LONG: Password length must be of 8-30 and alphanumeric.',
+                'warning',
+            )
+        } else {
+            // all checks are passed we could login the user if the credentials are correct after
+            // making api request to the backend
+            makeApiRequestPOST(SIGN_IN_ROUTE_ENDPOINT, {
+                user: user,
+                password: password,
+            })
+                .then(res => {
+                    const {code} = res.data
+                    if (code === 'PROVIDED_INCOMPLETE_DATA') {
+                        prompt('Something went wrong in your end.', 'warning')
+                    } else if (res.data.code === 'EMAIL_LENGTH_EXCEED') {
+                        prompt(
+                            'The account with this detail is blocked/deleted or may be now allowed.',
+                            'warning',
+                        )
+                    } else if (
+                        res.data.code === 'SHORT_PASSWORD' ||
+                        res.data.code === 'PASSWORD_LENGTH_EXCEED'
+                    ) {
+                        prompt('Wrong password. Please try again.', 'warning')
+                    } else if (res.data.code === 'SIGNIN_FAILED') {
+                        prompt(
+                            'Something went wrong. Please try again.',
+                            'error',
+                        )
+                    } else if (res.data.code === 'USER_NOT_FOUND') {
+                        prompt('No user account found with this data.', 'error')
+                    } else if (res.data.code === 'WRONG_PASSWORD') {
+                        prompt('Wrong password. Please try again.', 'warning')
+                    } else if (res.data.code === 'USER_ACCOUNT_IS_DISABLED') {
+                        prompt(
+                            'This account is disabled and not allowed to login.',
+                            'warning',
+                        )
+                    } else if (res.data.code === 'SUCCESS') {
+                        prompt('User successfully logged in.', 'success')
+                        setLocalUserData(JSON.stringify(res.data.data))
+                    }
+                })
+                .catch(_err => {
+                    prompt('Something went wrong. Please try again.', 'error')
+                })
+        }
     }
 
     return (
@@ -42,12 +135,6 @@ const Login = (props: LoginProps) => {
                 useAngle={true}
                 style={{width: '100%', flex: 1}}
                 colors={themeColors.backgroundgradient}>
-                <SobyteAlertBox
-                    visible={visible}
-                    setVisibility={setVisibility}
-                    onlyConfirmButton
-                />
-
                 <Header
                     navigation={props.navigation}
                     headerTitle="Sign In"
@@ -57,8 +144,8 @@ const Login = (props: LoginProps) => {
                 <KeyboardAvoidingView behavior="padding" style={{flex: 1}}>
                     <PaddingView padding="extra">
                         <SobyteTextInput
-                            value={email}
-                            onChangeText={e => setEmail(e)}
+                            value={user}
+                            onChangeText={e => setUser(e)}
                             keyboardType="email-address"
                             placeholder="Email"
                             autoCompleteType="email"
@@ -66,11 +153,23 @@ const Login = (props: LoginProps) => {
                             secureTextEntry={false}
                             returnKeyType="go"
                         />
+                        <SobyteTextInput
+                            value={password}
+                            onChangeText={e => setPassword(e)}
+                            placeholder="Password"
+                            returnKeyType="go"
+                            onSubmitEditing={() => LoginUser()}
+                            secureTextEntry={true}
+                            reversedColor
+                        />
 
                         <ScalerAuthButton
                             simpleGradientColor
-                            title="Get OTP"
-                            onPress={() => LoginUser()}
+                            title="Sign In"
+                            onPress={() => {
+                                Keyboard.dismiss()
+                                LoginUser()
+                            }}
                             gradient
                             scale={0.95}
                         />
