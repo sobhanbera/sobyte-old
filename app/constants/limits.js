0ba0e1a96444ad1c6c8ffb968add1b3fa2192954
sobhanbera <sobhanbera258@gmail.com>
2021-08-01 19:12:01 +0530

    limits

--- app/constants/limits.js
+++ app/constants/limits.js
@@ -3,6 +3,16 @@
  * max length, min length and such things...
  */
 
+// authentication constants..
 export const MAX_EMAIL_LENGTH = 320
 export const MAX_USERNAME_LENGTH = 30
+export const MIN_USERNAME_LENGTH = 8
 export const MAX_PASSWORD_LENGTH = 30
+export const MIN_PASSWORD_LENGTH = 8
+
+// limit of connection to mysql at once...
+export const MAX_SQL_CONCURRENT_CONNECTION = 1000000000000 // default mysql connection limit is 10 at once
+
+export const HASHING_MAX_SALT = 10 // the max salt for hashing string (mainly for password)
+
+export default 'constants for limits'
