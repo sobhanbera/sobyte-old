29f111d43b116a59b7ef597f12bd2a4574031ca3
sobhanbera <sobhanbera258@gmail.com>
2021-08-01 19:12:10 +0530

    constants updated

--- app/constants/index.js
+++ app/constants/index.js
@@ -125,7 +125,11 @@ export const EMAIL_REGEX =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 export const IP_ADDRESS_REGEX =
     /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+export const ALPHA_NUMERIC_PASSWORD_REGEX = /^[a-zA-Z0-9]+$/
 
 // this variable will not be used since there is not use of this but instead to recognize some
 // patern of backend this variable is here...
 export const bannedIPs = ['127.0.0.0', '0.0.0.0', 'localhost']
+
+export const PASSWORD_CHARACTERS =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz@$%^&*(){}[]-_+|<>'
