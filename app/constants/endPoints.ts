e8e7dcda69f8d6661b762401ffe1bc4aa35a3dae
sobhanbera <sobhanbera258@gmail.com>
2021-08-02 00:03:40 +0530

    typo

--- app/constants/endPoints.ts
+++ app/constants/endPoints.ts
@@ -1,12 +1,12 @@
 // main api endpoint url...
-const MAIN_API_ENDPOINT = 'http://localhost:8000'
+const MAIN_API_ENDPOINT = 'http://192.168.29.184:8000'
 export default MAIN_API_ENDPOINT
 
 // other sub routes of the api endpoint are...
 // authentication routes
 export const SIGN_IN_ROUTE_ENDPOINT = '/authentication/signin'
 export const SIGN_UP_ROUTE_ENDPOINT = '/authentication/signup'
-export const UPDATE_RRRLAST_LOGIN_ROUTE_ENDPOINT =
+export const UPDATE_LAST_LOGIN_ROUTE_ENDPOINT =
     '/authentication/updatelastlogin'
 
 // accounts related routes
