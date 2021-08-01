7d50b1d916e181aeeb50c8c8f46f5b7cd4b9da6c
sobhanbera <sobhanbera258@gmail.com>
2021-08-01 13:07:36 +0530

    main navigation fully changed

--- app/controller/AppStartingPoint.tsx
+++ app/controller/AppStartingPoint.tsx
@@ -1,22 +1,23 @@
-import React, {useState} from 'react'
-import {NavigationContainer, DarkTheme} from '@react-navigation/native'
+import React from 'react'
 
-import Player from '../api/PlayerControls'
-import MusicApi from '../api'
-import ThemeProvider from '../themes/ThemeProvider'
-import MusicFetcher from '../api/MusicFetcher'
-
-import AuthenticationNavigation from './Authentication'
-import AppNavigation from './AppInside'
-
-import {FullScreenLoading, Prompt} from '../components'
-import SettingsProvider from '../context/Settings'
 import AppMainBackendApi from '../backend'
+import MusicApi from '../api'
+import MusicFetcher from '../api/MusicFetcher'
+import ThemeProvider from '../themes/ThemeProvider'
+import SettingsProvider from '../context/Settings'
+import Player from '../api/PlayerControls'
+import {Prompt} from '../components'
 
-const AppStartingPoint = () => {
-    const [userLoggedIn, setUserLoggedIn] = useState<boolean>(true) // initial value must be false... true only for development purpose
-    const [loading, setLoading] = useState<boolean>(false)
+import AppLaunchingNavigation from './AppLaunchingNavigation'
 
+/**
+ * application's main context api component renderer
+ * or the app's main store/state
+ */
+interface ContextAPIsRendererProps {
+    children?: React.ReactChild
+}
+function AppStartingPoint(props: ContextAPIsRendererProps) {
     return (
         <AppMainBackendApi>
             <MusicApi>
@@ -25,14 +26,7 @@ const AppStartingPoint = () => {
                         <SettingsProvider>
                             <Player>
                                 <Prompt>
-                                    <NavigationContainer theme={DarkTheme}>
-                                        {!userLoggedIn ? (
-                                            <AuthenticationNavigation />
-                                        ) : (
-                                            <AppNavigation />
-                                        )}
-                                        <FullScreenLoading visible={loading} />
-                                    </NavigationContainer>
+                                    <AppLaunchingNavigation />
                                 </Prompt>
                             </Player>
                         </SettingsProvider>
