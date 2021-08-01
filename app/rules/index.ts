3f5a70e613abef48dc8ac580b5992bf2ab91721d
sobhanbera <sobhanbera258@gmail.com>
2021-08-01 13:47:37 +0530

    new rules

--- app/rules/index.ts
+++ app/rules/index.ts
@@ -1,3 +1,40 @@
+/**
+ * © 2010 Sobyte
+ *
+ * @author : Sobhan Bera (sobhanbera)
+ * @other_editors :
+ * @file : Typescript
+ *
+ * Purpose - for denoting the rules of developing the application...
+ */
+
+/** RULE: 1 ********************************************************************************************************************/
+//
+//
+// IMP COPYRIGHT COMMENT INFORMATION IN
+// EACH NEW FILE
+//
+//
+// whevever creating a new file the file should consists the below multiline comment
+// and the information used in it are as follows:
+// author will be the name of the programmer who created that file and written something
+// other_editors will be the names of all other programmers who updated atleast 1 line of code
+// file will be the extension of that file
+// Purpose will be the reason why this file is needed. and what this file is doing...
+// more data could also be provided like date, time, location, etc..
+
+/**
+ * © 2010 Sobyte
+ *
+ * @author : Sobhan Bera (sobhanbera)
+ * @other_editors :
+ * @file : Typescript
+ *
+ * Purpose - renders the authentication flow of the application
+ */
+
+/**********************************************************************************************************************/
+
 /** @animation - Invariant Violation: You can define one of bounciness/speed, tension/friction,
  *  or stiffness/damping/mass,
  *  but not more than one */
