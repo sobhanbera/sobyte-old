bd2cb2cbcafd321be4125aa863884f2547fdb8bb
sobhanbera <sobhanbera258@gmail.com>
2021-08-05 21:08:48 +0530

    added like animation in music player

--- app/screens/main/MusicPlayer.tsx
+++ app/screens/main/MusicPlayer.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback} from 'react'
+import React, {FC, useState, useEffect, useCallback, useRef} from 'react'
 import {
     Text,
     View,
@@ -22,21 +22,27 @@ import {FetchedSongObject, SongObject} from '../../interfaces'
 import MusicPlayerSongCardView from '../../components/MusicPlayerSongCardView'
 import BackgroundBluredImage from '../../components/MusicPlayerSongCardView/BackgroundBluredImage'
 import {ListRenderItem} from 'react-native'
+import LottieView from 'lottie-react-native'
 
 const {width, height} = Dimensions.get('screen')
+
+const LikeAnimation = require('../../assets/animations/like.json')
+const PopupLikeAnimation = require('../../assets/animations/like_popup.json')
 
 interface PlayerProps {
     navigation?: any
 }
-const Player: React.FC<PlayerProps> = _props => {
-    const {current, play, playSongAtIndex, getTheIndexOfCurrentSong} =
-        usePlayer()
+const Player: FC<PlayerProps> = _props => {
+    // const {current, play, playSongAtIndex, getTheIndexOfCurrentSong} =
+    //     usePlayer()
     const {themeColors} = useTheme()
     const {initMusicApi, search, error} = useMusicApi()
     const [songs, setSongs] = useState<FetchedSongObject>()
 
-    const scrollX = React.useRef(new Animated.Value(0)).current
-    const scrollReference = React.useRef<any>(null)
+    const likeAnimRef = useRef<LottieView>(null)
+
+    const scrollX = useRef(new Animated.Value(0)).current
+    const scrollReference = useRef<any>(null)
 
     const initializeMusicPlayer = () => {
         search('most listened bollywood songs', 'SONG')
@@ -65,29 +71,15 @@ const Player: React.FC<PlayerProps> = _props => {
             })
     }, [error])
 
-    /**
-     * we are calling the loadData -(which loads all the data for explore tab) function twice
-     * because it may not be ready or compiled when we call it for the first time so indented calling
-     * also with a fallback variable error whenever it changed again everything will load from beginning
-     * this function is also used in explore tab UI
-     */
+    const playLikeAnimation = () => {
+        likeAnimRef.current?.play(0, 90)
+        setTimeout(() => {
+            likeAnimRef.current?.reset()
+        }, 2000)
+    }
     useEffect(() => {
-        initMusicApi()
-            .then(() => {
-                initMusicApi()
-                    .then(() => {
-                        // loadInitialMusicPlayerData()
-                    })
-                    .catch(() => {})
-            })
-            .catch(() => {
-                // this will only be called when the internet connectivity is very slow or not present...
-                console.error(
-                    '(Outer) Error Initiating Music Api... no internet connection found',
-                )
-            })
-    }, [error])
-
+        playLikeAnimation()
+    }, [])
     // useEffect(() => {
     // console.log('CURRENT INDEX 1', getTheIndexOfCurrentSong())
     // const playbackQueueEnded = TrackPlayer.addEventListener(
@@ -190,44 +182,69 @@ const Player: React.FC<PlayerProps> = _props => {
                 flex: 1,
                 justifyContent: 'center',
                 alignItems: 'center',
+                width: '100%',
             }}>
-            <View
-                style={{
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    width: '100%',
-                    flex: 1,
-                }}>
-                {songs?.content.length &&
-                songs.content[0].musicId.length > 0 ? (
-                    <Animated.FlatList
-                        data={songs.content}
-                        renderItem={renderItem}
-                        keyExtractor={keyExtractor}
-                        getItemLayout={getItemLayout}
-                        ref={scrollReference}
-                        pagingEnabled={true}
-                        scrollEventThrottle={16}
-                        scrollToOverflowEnabled
-                        overScrollMode={'always'}
-                        bounces={true}
-                        horizontal={true}
-                        showsHorizontalScrollIndicator={false}
-                        showsVerticalScrollIndicator={false}
+            {songs?.content.length && songs.content[0].musicId.length > 0 ? (
+                <Animated.FlatList
+                    data={songs.content}
+                    renderItem={renderItem}
+                    keyExtractor={keyExtractor}
+                    getItemLayout={getItemLayout}
+                    ref={scrollReference}
+                    pagingEnabled={true}
+                    scrollEventThrottle={16}
+                    scrollToOverflowEnabled
+                    overScrollMode={'always'}
+                    bounces={true}
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    showsVerticalScrollIndicator={false}
+                    // onScroll={Animated.event(
+                    //     [{nativeEvent: {contentOffset: {x: scrollX}}}],
+                    //     {
+                    //         useNativeDriver: true,
+                    //         listener: event => {
+                    //             scrollChangedHandler(event)
+                    //         },
+                    //     },
+                    // )}
+                />
+            ) : (
+                <View
+                    style={{
+                        flex: 1,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: '100%',
+                        height: '100%',
+                    }}>
+                    <Image
+                        source={{uri: APP_LOGO_LINK}}
+                        style={[
+                            {
+                                opacity: 1,
+                                width: 218 / 2,
+                                height: 276 / 2,
+                            },
+                        ]}
+                        // blurRadius={IMAGE_BLUR_RADIUS / 5000}
                     />
-                ) : null}
-            </View>
-            {/* these are the temporary data and are not reqired that much as the main UI component itself... */}
-            {/* <Image
-                source={{uri: current.artwork || APP_LOGO_LINK}}
-                style={[
-                    StyleSheet.absoluteFillObject,
-                    {
-                        opacity: 0,
-                    },
-                ]}
-            /> */}
+                </View>
+            )}
+
+            <LottieView
+                ref={likeAnimRef}
+                source={PopupLikeAnimation}
+                style={{
+                    position: 'absolute',
+                    // backgroundColor: 'white',
+                }}
+                autoSize={false}
+                autoPlay={false}
+                cacheStrategy="strong"
+                loop={false}
+            />
+
             {/* main content of this particular tab */}
             {/* {songs.length > 0 && songs[0].artwork.length ? (
                 <View style={StyleSheet.absoluteFillObject}>
@@ -245,59 +262,6 @@ const Player: React.FC<PlayerProps> = _props => {
             ) : null} */}
 
             {/* <View
-                style={{
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    width: '100%',
-                    flex: 1,
-                }}>
-                {songs.length && songs[0].artwork.length > 0 ? (
-                    <Animated.ScrollView
-                        onScroll={Animated.event(
-                            [{nativeEvent: {contentOffset: {x: scrollX}}}],
-                            {
-                                useNativeDriver: true,
-                                listener: event => {
-                                    scrollChangedHandler(event)
-                                },
-                            },
-                        )}
-                        {songs.map((song, index) => {
-                            return (
-                                <MusicPlayerSongCardView
-                                    key={`${song.id}-${index}`}
-                                    item={song}
-                                    index={index}
-                                    scrollX={scrollX}
-                                />
-                            )
-                        })}
-                    </Animated.ScrollView>
-                ) : (
-                    <View
-                        style={{
-                            flex: 1,
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            width: '100%',
-                            height: '100%',
-                        }}>
-                        <Image
-                            source={{uri: APP_LOGO_LINK}}
-                            style={[
-                                {
-                                    opacity: 1,
-                                    width: 218 / 2,
-                                    height: 276 / 2,
-                                },
-                            ]}
-                            // blurRadius={IMAGE_BLUR_RADIUS / 5000}
-                        />
-                    </View>
-                )} */}
-
-            {/* <View
                     style={{
                         // height: 100,
                         justifyContent: 'center',
@@ -313,8 +277,6 @@ const Player: React.FC<PlayerProps> = _props => {
                         color={themeColors.themecolorrevert[0]}
                     />
                 </View> */}
-
-            {/* </View> */}
         </View>
     )
 }
