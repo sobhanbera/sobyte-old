e99b9d25c895bbd768f7cb9110f20e9e03df07ec
sobhanbera <sobhanbera258@gmail.com>
2021-08-06 23:58:04 +0530

    lastly played added in music featcher context api

--- app/api/MusicFetcher.tsx
+++ app/api/MusicFetcher.tsx
@@ -5,6 +5,13 @@ import musicFetch from 'react-native-ytdl'
 
 import {AUDIO_QUALITY_STORAGE_KEY} from '../constants'
 
+/**
+ * this is the previously listened songs list
+ * if any song is played once than no need to retrive the data
+ * source for it again and again
+ */
+let previouslyLoadedSongs: {[key: string]: string} = {}
+
 type AudioType =
     | 'highest'
     | 'lowest'
@@ -52,8 +59,27 @@ const MusicFetcher: React.FC<MusicFetcherProps> = props => {
 
     async function fetchMusic(id: string, _quality: AudioType = quality) {
         return new Promise(async (resolve, _reject) => {
+            console.log(
+                'the save data is if it is saved than:: ',
+                previouslyLoadedSongs[id],
+            )
+            /**
+             * if the songs is already played than providing the previously stored data
+             * source of that particular songs/track
+             * in this way the data would be used less and also make the app somewhat more performant
+             * than previous
+             */
+            if (
+                previouslyLoadedSongs[id] !== null &&
+                previouslyLoadedSongs[id] !== undefined
+            ) {
+                console.log('PROVIDED FROM I')
+                return resolve(previouslyLoadedSongs[id])
+            }
             const URL = getURL(id)
             const result = await musicFetch(URL, {quality: _quality})
+            // saving the data to use later on if the same song is played with the same musicID
+            previouslyLoadedSongs[id] = result[0].url
 
             /** since the result will provide a object like this:-
              * result = [{
