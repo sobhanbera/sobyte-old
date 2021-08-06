c33078716d43960a00ea21cf48c19f6a85735414
sobhanbera <sobhanbera258@gmail.com>
2021-08-06 19:25:25 +0530

    blurred bg image improved

--- app/components/MusicPlayerSongCardView/BackgroundBluredImage.tsx
+++ app/components/MusicPlayerSongCardView/BackgroundBluredImage.tsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import {StyleSheet, Animated, Dimensions} from 'react-native'
 
-import {Track} from '../../api/PlayerControls'
 import {APP_LOGO_LINK} from '../../constants'
 
 const {width} = Dimensions.get('screen')
 const IMAGE_BLUR_RADIUS = 50
 
 interface Props {
-    song: Track
+    image: string
     scrollX: Animated.Value
     index: number
 }
 const BackgroundBluredImage = (props: Props) => {
-    const {song, scrollX, index} = props
+    const {image, scrollX, index} = props
     const inputRange = [(index - 1) * width, index * width, (index + 1) * width]
     const opacity = scrollX.interpolate({
         inputRange: inputRange,
@@ -22,14 +21,13 @@ const BackgroundBluredImage = (props: Props) => {
 
     return (
         <Animated.Image
-            key={`${song.id}-${index}`}
             style={[
                 StyleSheet.absoluteFillObject,
                 {
                     opacity: opacity,
                 },
             ]}
-            source={{uri: song.artwork || APP_LOGO_LINK}}
+            source={{uri: image || APP_LOGO_LINK}}
             blurRadius={IMAGE_BLUR_RADIUS}
         />
     )
