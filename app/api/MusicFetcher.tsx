991ad2f5ad8528934712b9b02986485090380919
sobhanbera <sobhanbera258@gmail.com>
2021-08-07 17:47:22 +0530

    optimizing and updating the new code values of audio quality code

--- app/api/MusicFetcher.tsx
+++ app/api/MusicFetcher.tsx
@@ -1,9 +1,10 @@
 import React, {useContext, useState} from 'react'
 import {useEffect} from 'react'
 import AsyncStorage from '@react-native-community/async-storage'
-import musicFetch from 'react-native-ytdl'
 
 import {AUDIO_QUALITY_STORAGE_KEY} from '../constants'
+import {getTrackURL} from '../utils/Music'
+import {AudioQualityType} from '../interfaces'
 
 /**
  * this is the previously listened songs list
@@ -12,37 +13,28 @@ import {AUDIO_QUALITY_STORAGE_KEY} from '../constants'
  */
 let previouslyLoadedSongs: {[key: string]: string} = {}
 
-type AudioType =
-    | 'highest'
-    | 'lowest'
-    | 'highestaudio'
-    | 'lowestaudio'
-    | 'highestvideo'
-    | 'lowestvideo'
 const MusicFetcherContext = React.createContext({
-    fetchMusic: (_id: string, _quality: AudioType = 'highestaudio') =>
+    fetchMusic: (_id: string, _quality: AudioQualityType = 'extreme') =>
         new Promise((_resolve, _reject) => {}),
-    getURL: (_id: string) => {},
 })
 interface MusicFetcherProps {}
 const MusicFetcher: React.FC<MusicFetcherProps> = props => {
-    const [quality, setQuality] = useState<AudioType>('highestaudio')
+    const [quality, setQuality] = useState<AudioQualityType>('extreme')
 
     const loadQualityData = async () => {
         AsyncStorage.getItem(AUDIO_QUALITY_STORAGE_KEY)
-            .then(res => {
-                if (res === null || res === 'a' || res === 'e') {
-                    AsyncStorage.setItem(AUDIO_QUALITY_STORAGE_KEY, 'e')
+            .then((res: AudioQualityType | any) => {
+                if (!res) {
+                    AsyncStorage.setItem(AUDIO_QUALITY_STORAGE_KEY, 'extreme')
                         .then(res => {
                             if (res === null) {
                             }
                         })
                         .catch(_err => {})
-                    setQuality('highestaudio')
-                } else if (res === 'g') {
-                    setQuality('lowestaudio')
+                    setQuality('extreme')
                 } else {
-                    setQuality('lowest')
+                    setQuality(res)
+                    console.log(res)
                 }
             })
             .catch(_err => {})
@@ -52,17 +44,11 @@ const MusicFetcher: React.FC<MusicFetcherProps> = props => {
         loadQualityData()
     }, [])
 
-    function getURL(id: string) {
-        const origin = 'https://www.youtube.com/watch?v='
-        return origin + id
-    }
-
-    async function fetchMusic(id: string, _quality: AudioType = quality) {
+    async function fetchMusic(
+        id: string,
+        _quality: AudioQualityType = quality,
+    ) {
         return new Promise(async (resolve, _reject) => {
-            console.log(
-                'the save data is if it is saved than:: ',
-                previouslyLoadedSongs[id],
-            )
             /**
              * if the songs is already played than providing the previously stored data
              * source of that particular songs/track
@@ -76,26 +62,28 @@ const MusicFetcher: React.FC<MusicFetcherProps> = props => {
                 console.log('PROVIDED FROM I')
                 return resolve(previouslyLoadedSongs[id])
             }
-            const URL = getURL(id)
-            const result = await musicFetch(URL, {quality: _quality})
-            // saving the data to use later on if the same song is played with the same musicID
-            previouslyLoadedSongs[id] = result[0].url
+            getTrackURL(id)
+                .then(res => {
+                    // saving the data to use later on if the same song is played with the same musicID
+                    previouslyLoadedSongs[id] = res
 
-            /** since the result will provide a object like this:-
-             * result = [{
-             *     "header": [],
-             *     "url": string
-             * }]
-             * so we are returning the url value directly
-             */
-            // return result[0].url
-            resolve(result[0].url)
+                    /** since the result will provide a object like this:-
+                     * result = [{
+                     *     "header": [],
+                     *     "url": string
+                     * }]
+                     * so we are returning the url value directly
+                     */
+                    return resolve(res)
+                })
+                .catch(err => {
+                    console.log('MUSIC FETCHER ERR', err)
+                })
         })
     }
 
     const musicFetcherValues = {
         fetchMusic: fetchMusic,
-        getURL: getURL,
     }
     return (
         <MusicFetcherContext.Provider value={musicFetcherValues}>
