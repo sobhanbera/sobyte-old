9713dda3782f415c1023067077df17e517639be5
sobhanbera <sobhanbera258@gmail.com>
2021-08-08 19:26:49 +0530

    added new file for some component constants

---
+++ app/constants/components.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {View, ViewStyle, StatusBar, StatusBarPropsAndroid} from 'react-native'
+
+export const PaddingBottomView = (props: ViewStyle) => (
+    <View
+        style={[
+            {
+                paddingBottom: 120,
+                backgroundColor: 'transparent',
+            },
+            props,
+        ]}
+    />
+)
+export const DefaultStatusBarComponent = (props: StatusBarPropsAndroid) => (
+    <StatusBar
+        backgroundColor={'black'}
+        translucent={true}
+        barStyle="light-content"
+        {...props}
+    />
+)
