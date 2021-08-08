05d7951ab420bb4b1b9f16eb783d609ee1705c33
sobhanbera <sobhanbera258@gmail.com>
2021-08-09 00:46:14 +0530

    button for playlist and share also added

--- app/components/MusicPlayer/TrackButtonControls.tsx
+++ app/components/MusicPlayer/TrackButtonControls.tsx
@@ -1,8 +1,11 @@
 import React, {useEffect, useState} from 'react'
 import {View, StyleSheet} from 'react-native'
+
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+
 import TrackPlayer, {STATE_PLAYING} from 'react-native-track-player'
 
 import {usePlayer} from '../../context'
@@ -47,7 +50,23 @@ const TrackButtonControls = (props: Props) => {
                 size={24}
                 color={props.isLiked ? LIKE_ICON_OR_TEXT_COLOR : props.color}
                 name={'heart'}
-                onPress={() => props.likeIsMusic()}
+                onPress={() => props.likeIsMusic()} // TODO like feature
+            />
+
+            <MaterialIcons
+                style={styles.icon}
+                size={24}
+                color={props.color}
+                name={'queue-music'}
+                onPress={() => {}} // TODO playlist feature
+            />
+
+            <Ionicons
+                style={styles.icon}
+                size={24}
+                color={props.color}
+                name={'share-outline'}
+                onPress={() => {}} // TODO share feature
             />
 
             <FontAwesome5
