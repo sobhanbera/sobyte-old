96a07782d4ce85d0c1e0fa6b3751aad666e86443
sobhanbera <sobhanbera258@gmail.com>
2021-08-09 00:16:14 +0530

    unused

--- app/screens/main/Explore/Explore.tsx
+++ app/screens/main/Explore/Explore.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from 'react'
-import {View, ScrollView, RefreshControl, StatusBar} from 'react-native'
+import {View, ScrollView, RefreshControl} from 'react-native'
 import {Text as BlockTitle} from 'react-native-paper'
 
 import {
