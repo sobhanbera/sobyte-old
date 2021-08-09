f8812fa83e2b6d31b85658d651883131c0eabfe8
sobhanbera <sobhanbera258@gmail.com>
2021-08-09 19:37:39 +0530

    prompt is removed from main context queue

--- app/controller/AppStartingPoint.tsx
+++ app/controller/AppStartingPoint.tsx
@@ -6,7 +6,6 @@ import MusicFetcher from '../api/MusicFetcher'
 import ThemeProvider from '../themes/ThemeProvider'
 import SettingsProvider from '../context/Settings'
 import Player from '../api/PlayerControls'
-import {Prompt} from '../components'
 
 import AppLaunchingNavigation from './AppLaunchingNavigation'
 
@@ -18,24 +17,23 @@ interface ContextAPIsRendererProps {
     children?: React.ReactChild
 }
 function AppStartingPoint(_props: ContextAPIsRendererProps) {
+    {
+        /* we need backend api everywhere so it is the parent context api */
+    }
     return (
-        // we need prompt in music fetcher context api
-        <Prompt>
-            {/* we need backend api everywhere so it is the next parent context api */}
-            <AppMainBackendApi>
-                <MusicApi>
-                    <MusicFetcher>
-                        <ThemeProvider>
-                            <SettingsProvider>
-                                <Player>
-                                    <AppLaunchingNavigation />
-                                </Player>
-                            </SettingsProvider>
-                        </ThemeProvider>
-                    </MusicFetcher>
-                </MusicApi>
-            </AppMainBackendApi>
-        </Prompt>
+        <AppMainBackendApi>
+            <MusicApi>
+                <MusicFetcher>
+                    <ThemeProvider>
+                        <SettingsProvider>
+                            <Player>
+                                <AppLaunchingNavigation />
+                            </Player>
+                        </SettingsProvider>
+                    </ThemeProvider>
+                </MusicFetcher>
+            </MusicApi>
+        </AppMainBackendApi>
     )
 }
 
