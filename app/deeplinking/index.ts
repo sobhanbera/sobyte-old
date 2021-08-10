ffa36311e812b4460c91d055b0effb54732f72ff
sobhanbera <sobhanbera258@gmail.com>
2021-08-10 23:56:37 +0530

    comments

--- app/deeplinking/index.ts
+++ app/deeplinking/index.ts
@@ -1,5 +1,10 @@
 import {LinkingOptions} from '@react-navigation/native'
 
+/**
+ * all types of the links which will trigger to
+ * give an option to open with sobyte in android
+ * while opening the following links
+ */
 export const DEEP_LINKING_HTTPS_LINK = 'https://sobytemusic.com/listen'
 export const DEEP_LINKING_HTTP_LINK = 'http://sobytemusic.com/listen'
 export const DEEP_LINKING_LOCAL_LINK = 'sobytemusic://listen”'
@@ -10,6 +15,11 @@ export const DEEP_LINKING_PREFIXES = [
     DEEP_LINKING_LOCAL_LINK,
 ]
 
+/**
+ * every path in this deep linking configuration are the
+ * related paths from AppLaunchingNavigation.tsx or simply
+ * from where the main NavigationContainer exists...
+ */
 const DeepLinkingConfig: LinkingOptions = {
     prefixes: DEEP_LINKING_PREFIXES,
     config: {
