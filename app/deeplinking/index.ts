ce78345d1ca08b5cd376f9b362f8ae8222419de2
sobhanbera <sobhanbera258@gmail.com>
2021-08-10 23:51:08 +0530

    deeplinks updated with new configs

--- app/deeplinking/index.ts
+++ app/deeplinking/index.ts
@@ -1,7 +1,17 @@
 import {LinkingOptions} from '@react-navigation/native'
 
+export const DEEP_LINKING_HTTPS_LINK = 'https://sobytemusic.com/listen'
+export const DEEP_LINKING_HTTP_LINK = 'http://sobytemusic.com/listen'
+export const DEEP_LINKING_LOCAL_LINK = 'sobytemusic://listen”'
+
+export const DEEP_LINKING_PREFIXES = [
+    DEEP_LINKING_HTTPS_LINK,
+    DEEP_LINKING_HTTP_LINK,
+    DEEP_LINKING_LOCAL_LINK,
+]
+
 const DeepLinkingConfig: LinkingOptions = {
-    prefixes: ['https://sobytemusic.com/android', 'sobytemusic://android'],
+    prefixes: DEEP_LINKING_PREFIXES,
     config: {
         screens: {
             Profile: {
