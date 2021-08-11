19fef3d904c35a636fe12d18b1189d429a745def
sobhanbera <sobhanbera258@gmail.com>
2021-08-12 00:31:48 +0530

    less emojis

--- app/constants/emojis.ts
+++ app/constants/emojis.ts
@@ -1,18 +1,12 @@
 export const SMILEY_EMOJIS_STRING =
-    '😀😬😁😂😃😄😅😆😇😉😊🙂🙃😋😌😍😘😗😙😚😜😝😛🤑🤓😎🤗😏😶😐😑😒🙄🤔😳😞😟😠😡😔😕🙁☹️😣😖😫😩😤😮😱😨😰😯😦😧😢😥😪😓😭😵😲🤐😷🤒🤕😴🤣🤤🤢🤧🤠🤡🤥🤩🤨🤯🤪🤮🤫🤭🧐'
+    '😀😬😁😂😃😄😅😆😇😉😊🙂🙃😋😌😍😘😗😙😚😜😝😛🤑🤓😎🤗😏😶😐😑😒🙄🤔😳😞😟😠😡😔😕🙁😣😖😫😩😤😮😱😨😰😯😦😧😢😥😪😓😭😵😲🤐😷🤒🤕😴'
 
-export const HAND_EMOJIS_STRING =
-    '🙌👏👋👍👊✊✌️👌✋💪🙏☝️👆👇👈👉🤘🤞🤙🤛🤜🤚🤝🤟🤲'
+export const HAND_EMOJIS_STRING = '🙌👏👋👍👊✊👌✋💪🙏👇👆👈👉🤘'
 
-export const ANIMALS_EMOJIS_STRING =
-    '🐶🐱🐭🐹🐰🐻🐼🐨🐯🦁🐮🐷🐽🐸🐙🐵🙈🙉🙊🐒🐔🐧🐦🐤🐣🐥🐺🐗🐴🦄🐝🐛🐌🐞🐜🕷🕸🦂🦀🐍🐢🐠🐟🐡🐬🐳🐋🐊🐆🐅🐃🐂🐄🐪🐫🐘🐐🐏🐑🐎🐖🐀🐁🐓🦃🕊🐕🐩🐈🐇🐿🐾🐉🐲🦍🦊🦌🦏🦇🦅🦆🦉🦎🦈🦐🦑🦋🦓🦒🦔🦕🦖'
-
-export const MUSIC_EMOJIS_STRING = '🎤🎧🎼🎹🎷🎺🎸🎻🥁'
-
-export const GAMES_EMOJIS_STRING = '🎮👾🎲🎰🎳'
+export const MUSIC_EMOJIS_STRING = '🎤🎧🎹🎷🎺🎸🎻'
 
 export const CLOCK_EMOJIS_STRING =
     '🕐🕑🕒🕓🕔🕕🕖🕗🕘🕙🕚🕛🕜🕝🕞🕟🕠🕡🕢🕣🕤🕥🕦🕧'
 
-const ALL_EMOJIS_STRING = `${SMILEY_EMOJIS_STRING}${HAND_EMOJIS_STRING}${ANIMALS_EMOJIS_STRING}${MUSIC_EMOJIS_STRING}${GAMES_EMOJIS_STRING}${CLOCK_EMOJIS_STRING}`
+const ALL_EMOJIS_STRING = `${SMILEY_EMOJIS_STRING}${HAND_EMOJIS_STRING}${MUSIC_EMOJIS_STRING}${CLOCK_EMOJIS_STRING}`
 export default ALL_EMOJIS_STRING
