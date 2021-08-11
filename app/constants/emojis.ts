3c2df8674877e4f9559671afeeffd5b8855e3df2
sobhanbera <sobhanbera258@gmail.com>
2021-08-12 00:14:29 +0530

    some emojis are removed

--- app/constants/emojis.ts
+++ app/constants/emojis.ts
@@ -2,12 +2,7 @@ export const SMILEY_EMOJIS_STRING =
     '😀😬😁😂😃😄😅😆😇😉😊🙂🙃😋😌😍😘😗😙😚😜😝😛🤑🤓😎🤗😏😶😐😑😒🙄🤔😳😞😟😠😡😔😕🙁☹️😣😖😫😩😤😮😱😨😰😯😦😧😢😥😪😓😭😵😲🤐😷🤒🤕😴🤣🤤🤢🤧🤠🤡🤥🤩🤨🤯🤪🤮🤫🤭🧐'
 
 export const HAND_EMOJIS_STRING =
-    '🙌👏👋👍👊✊✌️👌✋💪🙏☝️👆👇👈👉🖕🤘🖖✍️🤞🤙🤛🤜🤚🤝🤟🤲'
-
-export const BODY_PART_EMOJIS_STRING = '👄👅👂👃👁👀👤🗣🧠'
-
-export const PERSONS_EMOJIS_STRING =
-    '👶👦👧👨👩👱👴👵👲👳👮👷💂🕵🎅👼👸👰💇💆🚶🏃💃🤴🤵🤰🤶🤦🤷🕺🤺🤸🤼🤽🤾🤹🧒🧑🧓🧕🧔🤱👨‍⚕️👩‍⚕️👨‍🎓👩‍🎓👨‍🏫👩‍🏫👨‍⚖️👩‍⚖️👨‍🌾👩‍🌾👨‍🍳👩‍🍳👨‍🔧👩‍🔧👨‍🏭👩‍🏭👨‍💼👩‍💼👨‍🔬👩‍🔬👨‍💻👩‍💻👨‍🎤👩‍🎤👨‍🎨👩‍🎨👨‍✈️👩‍✈️👨‍🚀👩‍🚀👨‍🚒👩‍🚒👮‍♂️👮‍♀️🕵️‍♂️🕵️‍♀️💂‍♂️💂‍♀️👷‍♂️👷‍♀️👳‍♂️👳‍♀️👱‍♂️👱‍♀️🙍‍♂️🙍‍♀️🙎‍♂️🙎‍♀️🙅‍♂️🙅‍♀️🙆‍♂️🙆‍♀️💁‍♂️💁‍♀️🙋‍♂️🙋‍♀️🙇‍♂️🙇‍♀️🤦‍♂️🤦‍♀️🤷‍♂️🤷‍♀️💆‍♂️💆‍♀️💇‍♂️💇‍♀️'
+    '🙌👏👋👍👊✊✌️👌✋💪🙏☝️👆👇👈👉🤘🤞🤙🤛🤜🤚🤝🤟🤲'
 
 export const ANIMALS_EMOJIS_STRING =
     '🐶🐱🐭🐹🐰🐻🐼🐨🐯🦁🐮🐷🐽🐸🐙🐵🙈🙉🙊🐒🐔🐧🐦🐤🐣🐥🐺🐗🐴🦄🐝🐛🐌🐞🐜🕷🕸🦂🦀🐍🐢🐠🐟🐡🐬🐳🐋🐊🐆🐅🐃🐂🐄🐪🐫🐘🐐🐏🐑🐎🐖🐀🐁🐓🦃🕊🐕🐩🐈🐇🐿🐾🐉🐲🦍🦊🦌🦏🦇🦅🦆🦉🦎🦈🦐🦑🦋🦓🦒🦔🦕🦖'
@@ -16,20 +11,8 @@ export const MUSIC_EMOJIS_STRING = '🎤🎧🎼🎹🎷🎺🎸🎻🥁'
 
 export const GAMES_EMOJIS_STRING = '🎮👾🎲🎰🎳'
 
-export const VEHICLES_EMOJIS_STRING =
-    '🚗🚕🚙🚘🚖🚌🚍🚎🚐🛣🏎🏍🚲🚓🚑🚒🚔🚨🚚🚛🚜🚧⛽🚏🚦🚥🏁🛸🛴🛵🛑🛶'
-
-export const PLACES_EMOJIS_STRING =
-    '🎡🎢🎠🏗🌁🗼🏭⛲🎑🗽⛰🏔🗻🌋🏕⛺🏞🏜🏖🏝🌅🌄🌇🌆🏙🌃🌉🌌🌠🎇🎆🌈'
-
-export const BUILDINGS_EMOJIS_STRING =
-    '🏘🏰🏯🏟🏠🏡🏚🏢🏬🏣🏤🏥🏦🏨🏪🏫💒🏛⛪🕌🕍🕋⛩'
-
-export const FLAGS_EMOJIS_STRING =
-    '🇦🇫🇦🇽🇦🇱🇩🇿🇦🇸🇦🇩🇦🇴🇦🇮🇦🇶🇦🇬🇦🇷🇦🇲🇦🇼🇦🇺🇦🇹🇦🇿🇧🇸🇧🇭🇧🇩🇧🇧🇧🇾🇧🇪🇧🇿🇧🇯🇧🇲🇧🇹🇧🇴🇧🇶🇧🇦🇧🇼🇧🇷🇮🇴🇻🇬🇧🇳🇧🇬🇧🇫🇧🇮🇨🇻🇰🇭🇨🇲🇨🇦🇮🇨🇰🇾🇨🇫🇹🇩🇨🇱🇨🇳🇨🇽🇨🇨🇨🇴🇰🇲🇨🇬🇨🇩🇨🇰🇨🇷🇭🇷🇨🇺🇨🇼🇨🇾🇨🇿🇩🇰🇩🇯🇩🇲🇩🇴🇪🇨🇪🇬🇸🇻🇬🇶🇪🇷🇪🇪🇪🇹🇪🇺🇫🇰🇫🇴🇫🇯🇫🇮🇫🇷🇬🇫🇵🇫🇹🇫🇬🇦🇬🇲🇬🇪🇩🇪🇬🇭🇬🇮🇬🇷🇬🇱🇬🇩🇬🇵🇬🇺🇬🇹🇬🇬🇬🇳🇬🇼🇬🇾🇭🇹🇭🇳🇭🇰🇭🇺🇮🇸🇮🇳🇮🇩🇮🇷🇮🇶🇮🇪🇮🇲🇮🇱🇮🇹🇨🇮🇯🇲🇯🇵🇯🇪🇯🇴🇰🇿🇰🇪🇰🇮🇽🇰🇰🇼🇰🇬🇱🇦🇱🇻🇱🇧🇱🇸🇱🇷🇱🇾🇱🇮🇱🇹🇱🇺🇲🇴🇲🇰🇲🇬🇲🇼🇲🇾🇲🇻🇲🇱🇲🇹🇲🇭🇲🇶🇲🇷🇲🇺🇾🇹🇲🇽🇫🇲🇲🇩🇲🇨🇲🇳🇲🇪🇲🇸🇲🇦🇲🇿🇲🇲🇳🇦🇳🇷🇳🇵🇳🇱🇳🇨🇳🇿🇳🇮🇳🇪🇳🇬🇳🇺🇳🇫🇲🇵🇰🇵🇳🇴🇴🇲🇵🇰🇵🇼🇵🇸🇵🇦🇵🇬🇵🇾🇵🇪🇵🇭🇵🇳🇵🇱🇵🇹🇵🇷🇶🇦🇷🇪🇷🇴🇷🇺🇷🇼🇧🇱🇸🇭🇰🇳🇱🇨🇵🇲🇻🇨🇼🇸🇸🇲🇸🇹🇸🇦🇸🇳🇷🇸🇸🇨🇸🇱🇸🇬🇸🇽🇸🇰🇸🇮🇸🇧🇸🇴🇿🇦🇬🇸🇰🇷🇸🇸🇪🇸🇱🇰🇸🇩🇸🇷🇸🇿🇸🇪🇨🇭🇸🇾🇹🇼🇹🇯🇹🇿🇹🇭🇹🇱🇹🇬🇹🇰🇹🇴🇹🇹🇹🇳🇹🇷🇹🇲🇹🇨🇹🇻🇺🇬🇺🇦🇦🇪🇬🇧🇺🇸🇻🇮🇺🇾🇺🇿🇻🇺🇻🇦🇻🇪🇻🇳🇼🇫🇪🇭🇾🇪🇿🇲🇿🇼🏳️‍🌈'
-
 export const CLOCK_EMOJIS_STRING =
     '🕐🕑🕒🕓🕔🕕🕖🕗🕘🕙🕚🕛🕜🕝🕞🕟🕠🕡🕢🕣🕤🕥🕦🕧'
 
-const ALL_EMOJIS_STRING = `${SMILEY_EMOJIS_STRING}${HAND_EMOJIS_STRING}${BODY_PART_EMOJIS_STRING}${PERSONS_EMOJIS_STRING}${ANIMALS_EMOJIS_STRING}${MUSIC_EMOJIS_STRING}${GAMES_EMOJIS_STRING}${VEHICLES_EMOJIS_STRING}${PLACES_EMOJIS_STRING}${BUILDINGS_EMOJIS_STRING}${FLAGS_EMOJIS_STRING}${CLOCK_EMOJIS_STRING}`
+const ALL_EMOJIS_STRING = `${SMILEY_EMOJIS_STRING}${HAND_EMOJIS_STRING}${ANIMALS_EMOJIS_STRING}${MUSIC_EMOJIS_STRING}${GAMES_EMOJIS_STRING}${CLOCK_EMOJIS_STRING}`
 export default ALL_EMOJIS_STRING
