bda18085bab9dfd6026221aaa1b8bdb8f6d85bdd
sobhanbera <sobhanbera258@gmail.com>
2021-08-12 00:48:11 +0530

    demo profile

--- app/screens/main/Profile/Profile.tsx
+++ app/screens/main/Profile/Profile.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import {SafeAreaView} from 'react-native'
+import {View} from 'react-native'
+import {ScrollView, Text} from 'react-native'
 
-import {GradientBackground, HeaderProfile} from '../../../components'
+import {GradientBackground, HeaderProfile, Scaler} from '../../../components'
 import {DefaultStatusBarComponent} from '../../../constants'
+import EMOJIS_STRING from '../../../constants/emojis'
 
 interface ProfileProps {
     navigation?: any
@@ -12,7 +14,48 @@ const Profile: React.FC<ProfileProps> = props => {
         <GradientBackground>
             <DefaultStatusBarComponent backgroundColor={'black'} />
 
-            <HeaderProfile navigation={props.navigation} />
+            <ScrollView
+                bounces
+                alwaysBounceVertical
+                bouncesZoom
+                scrollEventThrottle={16}
+                showsVerticalScrollIndicator={false}
+                showsHorizontalScrollIndicator={false}>
+                <HeaderProfile navigation={props.navigation} />
+
+                <View
+                    style={{
+                        flexWrap: 'wrap',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        paddingBottom: 100,
+                    }}>
+                    {/* {Array.from(EMOJIS_STRING.substring(0, 12)).map(item => {
+                        return (
+                            <Scaler
+                                onPress={() => {
+                                    console.log(item)
+                                }}>
+                                <Text
+                                    style={{
+                                        fontSize: 23,
+                                        padding: 6,
+                                        margin: 5,
+                                        backgroundColor: '#00000060',
+                                        borderRadius: 50,
+                                        width: 50,
+                                        height: 50,
+                                        textAlign: 'center',
+                                        textAlignVertical: 'center',
+                                    }}>
+                                    {item}
+                                </Text>
+                            </Scaler>
+                        )
+                    })} */}
+                </View>
+            </ScrollView>
         </GradientBackground>
     )
 }
