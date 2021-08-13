903aa697f5dec4b7fbbc31ef1acf85ad8ae60378
sobhanbera <sobhanbera258@gmail.com>
2021-08-13 18:57:47 +0530

    qr code testing code and will be removed to its separate component

--- app/screens/main/Profile/Profile.tsx
+++ app/screens/main/Profile/Profile.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {ToastAndroid, View} from 'react-native'
 import {ScrollView} from 'react-native'
+import QRCode from 'react-native-qrcode-svg'
 
 import {GradientBackground, HeaderProfile} from '../../../components'
-import {DefaultStatusBarComponent} from '../../../constants'
+import {
+    AppIconConstant,
+    DefaultStatusBarComponent,
+    DEFAULT_QR_CODE_IMAGE_SIZE,
+} from '../../../constants'
 
 interface ProfileProps {
     navigation?: any
@@ -20,6 +26,34 @@ const Profile: React.FC<ProfileProps> = props => {
                 showsVerticalScrollIndicator={false}
                 showsHorizontalScrollIndicator={false}>
                 <HeaderProfile navigation={props.navigation} />
+
+                {/* qr code generation and rendering view */}
+                <View
+                    style={{
+                        borderRadius: 2,
+                        overflow: 'hidden',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}>
+                    <QRCode
+                        value={'1'}
+                        ecl="H"
+                        color={'white'}
+                        backgroundColor={'#0000007F'}
+                        size={DEFAULT_QR_CODE_IMAGE_SIZE}
+                        // enableLinearGradient
+                        // linearGradient={['tranparent', 'tranparent']}
+                        // logoSize={36}
+                        // logo={AppIconConstant}
+                        onError={() => {
+                            // toast the message or a message showing cannot generate QR Code currently...
+                            ToastAndroid.show(
+                                "Can't generate QR Code currently. Sorry for your inconvenience.",
+                                ToastAndroid.SHORT,
+                            )
+                        }}
+                    />
+                </View>
             </ScrollView>
         </GradientBackground>
     )
