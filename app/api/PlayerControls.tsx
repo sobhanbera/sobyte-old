b663035a4c324e4796f475b35fb165a9f4c0c552
sobhanbera <sobhanbera258@gmail.com>
2021-08-15 23:31:07 +0530

    unused

--- app/api/PlayerControls.tsx
+++ app/api/PlayerControls.tsx
@@ -4,7 +4,6 @@ import {useRef} from 'react'
 import TrackPlayer, {STATE_PAUSED} from 'react-native-track-player'
 
 import {useApp, useFetcher} from '../context'
-import {ContinuationObject} from '../interfaces'
 
 /**
  * the data type or the object type for each Tracks of the song
