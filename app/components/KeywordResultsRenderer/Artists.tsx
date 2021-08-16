b10f9bbbdbedb9a330971b619fb935a630843ca9
sobhanbera <sobhanbera258@gmail.com>
2021-08-16 13:59:46 +0530

    separate component for each artists list in explore tab now
    this will make each of the api requests fast then now
    
    this is the component for artists

---
+++ app/components/KeywordResultsRenderer/Artists.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+interface Props {
+    keyword: string
+}
+const KeywordResultsRenderer_Artists = (props: Props) => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default KeywordResultsRenderer_Artists
