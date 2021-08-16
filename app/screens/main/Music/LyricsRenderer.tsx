ea9f5090a332c17131fb7b204563c02bf13bfde8
sobhanbera <sobhanbera258@gmail.com>
2021-08-16 21:48:33 +0530

    new screen only for showing lyrics

---
+++ app/screens/main/Music/LyricsRenderer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+interface Props {}
+const SongLyricsRenderer = (props: Props) => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default SongLyricsRenderer
