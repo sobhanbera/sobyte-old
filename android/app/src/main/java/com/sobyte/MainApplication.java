877b5c0dcf37397c611c2518a7450847c54debe0
sobhanbera <sobhanbera258@gmail.com>
2021-08-18 23:32:24 +0530

    linking of rn-fetch-blob by using
    RNFB_ANDROID_PERMISSIONS=true react-native link

--- android/app/src/main/java/com/sobyte/MainApplication.java
+++ android/app/src/main/java/com/sobyte/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.zoontek.rnpermissions.RNPermissionsPackage;
 import fr.greweb.reactnativeviewshot.RNViewShotPackage;
 import org.reactnative.camera.RNCameraPackage;
