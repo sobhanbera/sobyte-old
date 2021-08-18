dd6c67fb1a7a9e3eb10dbb5a333226c080a05005
sobhanbera <sobhanbera258@gmail.com>
2021-08-18 23:20:56 +0530

    android linking of two packages

--- android/app/src/main/java/com/sobyte/MainApplication.java
+++ android/app/src/main/java/com/sobyte/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import fr.greweb.reactnativeviewshot.RNViewShotPackage;
+import org.reactnative.camera.RNCameraPackage;
 import com.reactnativecommunity.slider.ReactSliderPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
