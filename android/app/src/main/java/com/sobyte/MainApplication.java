f00bccadea6d479ad55315580b5f8314dd41630a
sobhanbera <sobhanbera258@gmail.com>
2021-08-18 23:26:03 +0530

    android linking

--- android/app/src/main/java/com/sobyte/MainApplication.java
+++ android/app/src/main/java/com/sobyte/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.zoontek.rnpermissions.RNPermissionsPackage;
 import fr.greweb.reactnativeviewshot.RNViewShotPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.reactnativecommunity.slider.ReactSliderPackage;
