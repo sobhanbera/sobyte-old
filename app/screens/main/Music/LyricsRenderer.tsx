0580e80d9c6aa8771b221723edc0df0e6516232f
sobhanbera <sobhanbera258@gmail.com>
2021-08-19 23:02:09 +0530

    new lyrics testing

--- app/screens/main/Music/LyricsRenderer.tsx
+++ app/screens/main/Music/LyricsRenderer.tsx
@@ -75,62 +75,69 @@ export default SongLyricsRenderer
 
 // testing
 const LRC_STRING = `
-[ti:lut gaye]
-[ar:Jubin Nautiyal, Tanishk Bagchi, Nusrat Fateh Ali Khan]
-[al:Dont]
-[au:JN]
-[by:sobhanbera]
+[ti:wafa na raha sai]
+[ar:Jubin Nautiyaal]
+[al:Wafa Na Raha Sai]
+[au:Sobhan Bera]
+[by:Sobhan Bera]
 [la:HI]
 [ve:4.00]
 
-[00:00.76]Maine Jab Dekha Tha Tujhko
-[00:24.40]Raat Bhi Woh Yaad Hai Mujhko
-[00:27.00]Taare Ginte Ginte So Gaya
-[00:32.16]Dil Mera Dhadka Tha Kass Ke
-[00:34.81]Kuch Kaha Tha Tune Hass Ke
-[00:37.44]Main Usi Pal Tera Ho Gaya
-[00:42.78]Aasmano Pe Jo Khuda Hai
-[00:45.42]Usse Meri Yahi Dua Hai
-[00:47.98]Chand Yeh Har Roz Main Dekhu
-[00:50.92]Tere Sath Mein
-[00:52.63]Aankh Uthi Mohabbat Ne Angrai Li
-[00:57.71]Dil Ka Sauda Hua Chandani Raat Mein
-[01:02.73]Oh Teri Nazron Ne Kuch Aisa Jaadoo Kiya
-[01:06.51]Lut Gaye Hum Toh Pehli Mulaqaat Mein
-[01:13.81]Oh Aankh Uthi!
-[01:35.58]Paao Rakhna Na Zameen Par
-[01:37.76]Jaan Rukja Tu Ghadi Bhar
-[01:39.87]Thode Taare Toh Bichha Du
-[01:42.91]Main Tere Vaaste
-[01:45.52]Aajmale Mujhko Yaara
-[01:48.07]Tu Zara Sa Kar Ishaara
-[01:50.51]Dil Jala Ke Jagmaga Du
-[01:53.47]Main Tere Raaste
-[01:56.74]Haan Mere Jaisa Ishq Mein Pagal
-[01:59.18]Phir Mile Ya Na Mile Kal
-[02:01.86]Sochna Kya Hath Ye Dede
-[02:04.35]Mere Hath Mein
-[02:06.18]Aankh Uthi Mohabbat Ne Angrai Li
-[02:11.39]Dil Ka Sauda Hua Chandani Raat Mein
-[02:16.57]Oh Teri Nazron Ne Kuch Aisa Jaadoo Kiya
-[02:22.08]Lut Gaye Hum Toh Pehli Mulaqaat Mein
-[02:27.07]Oh Aankh Uthi!
-[02:38.05]Haan Kisse Mohabbat Ke
-[02:51.99]Hain Jo Kitabon Mein
-[02:54.69]Sab Chahta Hoon Main
-[02:57.27]Sang Tere Dohrana
-[02:59.90]Kitna Zaroori Hai
-[03:02.54]Ab Meri Khatir Tu
-[03:05.25]Mushqil Hai Mushqil Hai
-[03:08.27]Lafzon Mein Keh Pana
-[03:10.52]Ab Toh Yeh Alam Hai
-[03:13.16]Tu Jaan Mange Toh
-[03:15.64]Main Shaunk Se Dedu
-[03:18.06]Saugat Mein
-[03:20.52]Aankh Uthi Mohabbat Ne Angrai Li
-[03:24.93]Dil Ka Sauda Hua Chandani Raat Mein
-[03:30.24]Oh Teri Nazron Ne Kuch Aisa Jaadoo Kiya
-[03:35.95]Lut Gaye Hum Toh Pehli Mulaqaat Mein
-[03:41.01]Oh Aankh Uthi!
+[00:25.97]Roun Ya Hansun Teri Harkat Pe
+[00:35.68]Ya Phir Teri Taarif Karoon
+[00:41.17]Mere Dil Se Tu Aise Khel Gaya
+[00:44.50]Ab Jee Na Saku Mar Bhi Na Saku
+
+[00:50.32]Teri Zehar Bhari Do Aankhon Ki
+[00:55.20]Mujhe Chaal Samjh Mein Na Aayi
+
+[01:00.22]Wafa Na Raas Aayi
+[01:05.14]Tujhe O Harjai
+[01:07.26]Wafa Na Raas Aayi
+[01:13.59]Tujhe O Harjai
+
+[01:17.46]Sadiyon Yeh Zamana Yaad Rakhega
+[01:27.66]Yaar Teri Bewafai
+[01:32.00]Wafa Na Raas Aayee
+[01:35.53]Tujhe O Harjai
+
+[02:06.52]Waadon Ki Lashon Ko Bol Kahan Dafnaun
+[02:28.97]Waadon Ki Lashon Ko Bol Kahan Dafnaun
+[02:38.64]Khwabon Aur Yaadon Se Kaise Tumko Mitaun
+
+[02:44.60]Kyun Na Main Tujhe Pehchan Saka
+[02:52.88]Sach Tere Nahi Main Jaan Saka
+[02:57.92]Tere Noor Se Jo Raushan Tha Kabhi
+[03:02.81]Uss Shehar Mein Aag Lagai
+
+[03:05.69]Wafa Na Raas Aayi
+[03:13.91]Tujhe O Harjai
+[03:18.87]Wafa Na Raas Aayi
+[03:22.81]Tujhe O Harjai
+
+[03:30.24]Sadiyon Yeh Zamana Yaad Rakhega
+[03:35.34]Yaar Teri Bewafai
+[03:40.82]Wafa Na Raas Aayee
+[03:45.75]Tujhe O Harjai
+
+[03:46.69]Jis Jis Ko Mohabbat Raas Aayi
+[03:54.33]Woh Log Naseebon Wale The
+[03:56.75]Taqdeer Ke Hathon Haar Gaye
+[03:58.75]Hum Jaise Jo The
+
+[04:01.54]Sunn Yaar Mere Oh Harjai
+[04:03.79]Hum Thode Alag Dilwale The
+[04:06.28]Par Jaisa Socha Tha Humne
+[04:08.76]Tum Waise Na The
+
+[04:11.37]Tune Waar Kiya Sidhe Dil Pe
+[04:16.12]Aur Palak Bhi Na Jhapkayi
+
+[04:21.73]Wafa Na Raas Aayi
+[04:26.48]Tujhe O Harjai
+[04:32.55]Wafa Na Raas Aayi
+[04:37.22]Tujhe O Harjai
+
+
 
 `
