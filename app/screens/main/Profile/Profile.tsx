b7c88e5ee04cf8c93362ccb3fe4d8aeed700d7da
sobhanbera <sobhanbera258@gmail.com>
2021-09-24 22:28:28 +0530

    qr code removed from profile which was only for testing purpose

--- app/screens/main/Profile/Profile.tsx
+++ app/screens/main/Profile/Profile.tsx
@@ -1,25 +1,13 @@
 import React, {useState} from 'react'
-import {ToastAndroid, View} from 'react-native'
 import {ScrollView} from 'react-native'
-import QRCode from 'react-native-qrcode-svg'
-
-import QRCodeScanner from 'react-native-qrcode-scanner'
-import {RNCamera} from 'react-native-camera'
 
 import {GradientBackground, HeaderProfile} from '../../../components'
-import {
-    DefaultStatusBarComponent,
-    DEFAULT_QR_CODE_IMAGE_SIZE,
-} from '../../../constants'
-
-const QrCodeScanningAnimation = require('../../../assets/animations/qr_scanning.json')
+import {DefaultStatusBarComponent} from '../../../constants'
 
 interface ProfileProps {
     navigation?: any
 }
 const Profile: React.FC<ProfileProps> = props => {
-    const [pos, setPos] = useState<{x: number; y: number}>({x: 0, y: 0})
-
     return (
         <GradientBackground>
             <DefaultStatusBarComponent backgroundColor={'black'} />
@@ -32,66 +20,6 @@ const Profile: React.FC<ProfileProps> = props => {
                 showsVerticalScrollIndicator={false}
                 showsHorizontalScrollIndicator={false}>
                 <HeaderProfile navigation={props.navigation} />
-
-                {/* qr code generation and rendering view */}
-                <View
-                    style={{
-                        borderRadius: 2,
-                        overflow: 'hidden',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}>
-                    <QRCodeScanner
-                        onRead={() => {}}
-                        cameraType={'front'}
-                        cameraStyle={{}}
-                        cameraProps={{
-                            flashMode: 'on',
-                            onBarCodeRead: e => {
-                                // console.log('DATA', e)
-                                // alert(e.data)
-                            },
-                            onDoubleTap: e => {
-                                console.log(e)
-                                setPos(e)
-                            },
-                            autoFocus: 'on',
-                            focusDepth: 1,
-                        }}
-                        containerStyle={{
-                            position: 'relative',
-                        }}
-                    />
-                    <View
-                        style={{
-                            width: 10,
-                            height: 10,
-                            backgroundColor: '#4040ef',
-                            borderRadius: 100,
-                            top: pos.y,
-                            left: pos.x,
-                            position: 'absolute',
-                        }}
-                    />
-                    {/* <QRCode
-                        value={'68645582116258840.96939611'}
-                        ecl="H"
-                        color={'white'}
-                        backgroundColor={'#0000007F'}
-                        size={DEFAULT_QR_CODE_IMAGE_SIZE}
-                        // enableLinearGradient
-                        // linearGradient={['tranparent', 'tranparent']}
-                        // logoSize={36}
-                        // logo={AppIconConstant}
-                        onError={() => {
-                            // toast the message or a message showing cannot generate QR Code currently...
-                            ToastAndroid.show(
-                                "Can't generate QR Code currently. Sorry for your inconvenience.",
-                                ToastAndroid.SHORT,
-                            )
-                        }}
-                    /> */}
-                </View>
             </ScrollView>
         </GradientBackground>
     )
