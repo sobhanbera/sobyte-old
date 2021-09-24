b15b72c213dcab756a7374d695f4995683798356
sobhanbera <sobhanbera258@gmail.com>
2021-09-25 00:48:24 +0530

    language list added
    this list will be shown in the form of cards to choose a language and then those language's songs will be loaded when user launches app everytime

---
+++ app/constants/SongLanguages.ts
@@ -0,0 +1,10 @@
+/**
+ * list of langauges of songs
+ * for example hindi, english, bengali, tamil, punjabi, etc.
+ * this data will be shown while user logins to there account
+ * or via setting
+ * then after the songs will be played related to this choosed languages...
+ */
+const SongLanguagesList: {} = []
+
+export default SongLanguagesList
