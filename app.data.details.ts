28edd2b5a337b2481481e5b237a64ce7cd05c869
sobhanbera <sobhanbera258@gmail.com>
2021-10-10 21:17:35 +0530

    updated app details file

--- app.data.details.ts
+++ app.data.details.ts
@@ -2,8 +2,18 @@ export default {
     name: 'Sobyte',
     displayName: 'Sobyte',
     author: 'Sobhan Bera <https://github.com/sobhanbera>',
-    /** @version = String( @vc + "." + @vn ) */
-    version: 0.1,
+    /** @version = String( @vc + "." + @vn )
+     *
+     * this version if derived like this
+     *
+     * `major_change . updates . num_of_files . num_of_lines`
+     *
+     * 1. major_change => how many time major changes are done
+     * 2. updates => how many times app is updated
+     * 3. num_of_files => number of files under the app directory
+     * 4. num_of_lines => total number of lines in all the files in app directory
+     */
+    version: '1.0.142.22601',
     /** @vc - version code */
     vc: 0,
     /** @vn - version number */
