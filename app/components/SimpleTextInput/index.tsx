b66274921123d3a9d2250d56b5dedf85de1c18b1
sobhanbera <sobhanbera258@gmail.com>
2021-10-15 23:58:12 +0530

    updated textinput component

--- app/components/SimpleTextInput/index.tsx
+++ app/components/SimpleTextInput/index.tsx
@@ -1,27 +1,32 @@
 import React from 'react'
-import {View, TextInput} from 'react-native'
+import {TextInput, TextInputProps} from 'react-native'
 import {useTheme} from '../../context'
+import globalStyles from '../../styles/global.styles'
 
 interface Props {}
-
-const SimpleTextInput = (props: Props) => {
+const SimpleTextInput = (props: TextInputProps) => {
     const {placeholder} = useTheme().themeColors
 
     return (
-        <View>
-            <TextInput
-                style={{
+        <TextInput
+            focusable
+            clearTextOnFocus
+            placeholderTextColor={placeholder[0]}
+            selectionColor="white"
+            {...props}
+            // some props needs to be selected by line by line code
+            // or by the best sequencially
+            style={[
+                globalStyles.simpleTextInput,
+                {
                     borderColor: placeholder[0],
-                }}
-                focusable
-                clearTextOnFocus
-                placeholderTextColor={placeholder[0]}
-                selectionColor="white"
-                showSoftInputOnFocus={false}
-                // keyboard will not be shown when this input will be pressed since its a trigger to open an another screen
-                // and this component is a demo or dummy or virual button not text input any more
-            />
-        </View>
+                },
+                props.style,
+            ]}
+            showSoftInputOnFocus={false}
+            // keyboard will not be shown when this input will be pressed since its a trigger to open an another screen
+            // and this component is a demo or dummy or virual button not text input any more
+        />
     )
 }
 
