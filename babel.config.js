07eeab791094cd7d77943c17fcbd7ae2c627df52
sobhanbera <sobhanbera258@gmail.com>
2021-10-17 12:40:22 +0530

    babel config updated with env module

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,20 @@ module.exports = {
     presets: ['module:metro-react-native-babel-preset'],
 
     /** 'react-native-reanimated/plugin' should be placed last in the plugins list */
-    plugins: ['react-native-reanimated/plugin'],
+    // we are attaching the env variables modules
+    plugins: [
+        'react-native-reanimated/plugin',
+        [
+            'module:react-native-dotenv',
+            {
+                moduleName: '@env',
+                path: '.env',
+                blocklist: null,
+                allowlist: null,
+                safe: false,
+                allowUndefined: true,
+                verbose: false,
+            },
+        ],
+    ],
 }
