fe1abd179ac04f06c7085cd98dcfd1c17e1b2935
sobhanbera <sobhanbera258@gmail.com>
2021-10-20 21:52:48 +0530

    added the lang picker screen to main profile stack

--- app/controller/ProfileStack/index.tsx
+++ app/controller/ProfileStack/index.tsx
@@ -4,6 +4,7 @@ import {createStackNavigator} from '@react-navigation/stack'
 import Profile from '../../screens/main/Profile/Profile'
 import Setting from '../../screens/main/Profile/Setting'
 import SettingsUpdater from '../../screens/main/Profile/SettingsUpdater'
+import LanguagePicker from '../../screens/main/Profile/LanguagePicker'
 
 const ProfileStack = createStackNavigator()
 interface Props {}
@@ -18,6 +19,10 @@ const ProfileStackNavigator = (props: Props) => {
             initialRouteName="profile">
             <ProfileStack.Screen name="profile" component={Profile} />
             <ProfileStack.Screen name="setting" component={Setting} />
+            <ProfileStack.Screen
+                name="languagepicker"
+                component={LanguagePicker}
+            />
             <ProfileStack.Screen
                 name="settingsupdater"
                 component={SettingsUpdater}
