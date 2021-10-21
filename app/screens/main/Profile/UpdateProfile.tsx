111b1a309fbea9ddc96aa295efc1f4c929d6d32b
sobhanbera <sobhanbera258@gmail.com>
2021-10-21 22:51:36 +0530

    new screens to update profile

---
+++ app/screens/main/Profile/UpdateProfile.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+interface Props {
+    navigation?: any
+}
+const UpdateProfile = (props: Props) => {
+    return <View></View>
+}
+
+export default UpdateProfile
---
+++ app/screens/main/Profile/UpdateSocialMediaLinks.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+interface Props {
+    navigation?: any
+}
+
+const UpdateSocialMediaLinks = (props: Props) => {
+    return <View></View>
+}
+
+export default UpdateSocialMediaLinks
