4f60005a8a28b2df808be64484f9d3dd8d938234
sobhanbera <sobhanbera258@gmail.com>
2021-10-22 00:41:19 +0530

    new look in update social media links screen

--- app/screens/main/Profile/UpdateSocialMediaLinks.tsx
+++ app/screens/main/Profile/UpdateSocialMediaLinks.tsx
@@ -1,12 +1,114 @@
-import React from 'react'
-import {View, Text} from 'react-native'
+import React, {useState} from 'react'
+import {View} from 'react-native'
+import {useTranslation} from 'react-i18next'
+
+import {useSetting, useTheme, useUserData} from '../../../context'
+
+import globalStyles, {CustomGlobalStyles} from '../../../styles/global.styles'
+import {DefaultStatusBarComponent} from '../../../constants'
+import {AreaTitle, HeaderMain, SimpleTextInput} from '../../../components'
 
 interface Props {
     navigation?: any
 }
 
 const UpdateSocialMediaLinks = (props: Props) => {
-    return <View></View>
+    const {t} = useTranslation()
+    const {themeColors} = useTheme()
+    const {data} = useUserData()
+
+    const [facebookLink, setFacebookLink] = useState(data.facebook || '')
+    const [instagramLink, setInstagramLink] = useState(data.instagram || '')
+    const [githubLink, setGitHubLink] = useState(data.github || '')
+    const [linkedinLink, setLinkedinLink] = useState(data.linkedin || '')
+    const [snapchatLink, setSnapchatLink] = useState(data.snapchat || '')
+    const [twitterLink, setTwitterLink] = useState(data.twitter || '')
+
+    const commonTextInputStyle = [
+        globalStyles.border5,
+        globalStyles.smallPaddingHor,
+        CustomGlobalStyles(themeColors.themecolorrevert[0] + '45').borderColor,
+        CustomGlobalStyles(themeColors.themecolorrevert[0] + 'DF').color,
+    ]
+
+    return (
+        <View style={globalStyles.flex}>
+            <DefaultStatusBarComponent backgroundColor={'black'} />
+            <HeaderMain
+                navigation={props.navigation}
+                title={'Update Social Media Links'}
+                color={themeColors.themecolorrevert[0] + 'BF'}
+                backgroundColor={themeColors.themecolor[0]}
+                borderColor={themeColors.themecolorrevert[0] + '17'}
+            />
+
+            {/* facebook link update */}
+            <AreaTitle title={'Facebook Username'} notBold />
+            <SimpleTextInput
+                style={commonTextInputStyle}
+                value={facebookLink}
+                onChangeText={setFacebookLink}
+                placeholderTextColor={themeColors.placeholder[0] + 'AF'}
+                placeholder={'Enter Facebook Username'}
+                selectionColor={themeColors.themecolorrevert[0] + '7F'}
+            />
+
+            {/* instagram link update */}
+            <AreaTitle title={'Instagram Username'} notBold />
+            <SimpleTextInput
+                style={commonTextInputStyle}
+                value={instagramLink}
+                onChangeText={setInstagramLink}
+                placeholderTextColor={themeColors.placeholder[0] + 'AF'}
+                placeholder={'Enter Instagram Username'}
+                selectionColor={themeColors.themecolorrevert[0] + '7F'}
+            />
+
+            {/* github link update */}
+            <AreaTitle title={'GitHub Username'} notBold />
+            <SimpleTextInput
+                style={commonTextInputStyle}
+                value={githubLink}
+                onChangeText={setGitHubLink}
+                placeholderTextColor={themeColors.placeholder[0] + 'AF'}
+                placeholder={'Enter GitHub Username'}
+                selectionColor={themeColors.themecolorrevert[0] + '7F'}
+            />
+
+            {/* linkedin link update */}
+            <AreaTitle title={'Linkedin Username'} notBold />
+            <SimpleTextInput
+                style={commonTextInputStyle}
+                value={linkedinLink}
+                onChangeText={setLinkedinLink}
+                placeholderTextColor={themeColors.placeholder[0] + 'AF'}
+                placeholder={'Enter Linkedin Username'}
+                selectionColor={themeColors.themecolorrevert[0] + '7F'}
+            />
+
+            {/* snapchat link update */}
+            <AreaTitle title={'Snapchat Username'} notBold />
+            <SimpleTextInput
+                style={commonTextInputStyle}
+                value={snapchatLink}
+                onChangeText={setSnapchatLink}
+                placeholderTextColor={themeColors.placeholder[0] + 'AF'}
+                placeholder={'Enter Snapchat Username'}
+                selectionColor={themeColors.themecolorrevert[0] + '7F'}
+            />
+
+            {/* twitter link update */}
+            <AreaTitle title={'Twitter Username'} notBold />
+            <SimpleTextInput
+                style={commonTextInputStyle}
+                value={twitterLink}
+                onChangeText={setTwitterLink}
+                placeholderTextColor={themeColors.placeholder[0] + 'AF'}
+                placeholder={'Enter Twitter Username'}
+                selectionColor={themeColors.themecolorrevert[0] + '7F'}
+            />
+        </View>
+    )
 }
 
 export default UpdateSocialMediaLinks
