256112ed0c46bf6ad4d790bb58fa8e6c9f22451b
sobhanbera <sobhanbera258@gmail.com>
2021-10-21 22:52:25 +0530

    profile stack updated with new screen name constants
    with two new screens

--- app/controller/ProfileStack/index.tsx
+++ app/controller/ProfileStack/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import {createStackNavigator} from '@react-navigation/stack'
 
+import {
+    PROFILE_STACK__MAIN_PROFILE_SCREEN,
+    PROFILE_STACK__SETTINGS_SCREEN,
+    PROFILE_STACK__LANGUAGE_PICKER_SCREEN,
+    PROFILE_STACK__SETTINGS_UPDATER_SCREEN,
+    PROFILE_STACK__UPDATE_PROFILE_SCREEN,
+    PROFILE_STACK__UPDATE_SOCIAL_MEDIA_LINKS_SCREEN,
+} from '../../constants/screens'
 import Profile from '../../screens/main/Profile/Profile'
 import Setting from '../../screens/main/Profile/Setting'
 import SettingsUpdater from '../../screens/main/Profile/SettingsUpdater'
 import LanguagePicker from '../../screens/main/Profile/LanguagePicker'
+import UpdateProfile from '../../screens/main/Profile/UpdateProfile'
+import UpdateSocialMediaLinks from '../../screens/main/Profile/UpdateSocialMediaLinks'
 
 const ProfileStack = createStackNavigator()
 interface Props {}
@@ -16,16 +26,30 @@ const ProfileStackNavigator = (props: Props) => {
                 gestureEnabled: true,
                 gestureDirection: 'vertical',
             }}
-            initialRouteName="profile">
-            <ProfileStack.Screen name="profile" component={Profile} />
-            <ProfileStack.Screen name="setting" component={Setting} />
+            initialRouteName={PROFILE_STACK__MAIN_PROFILE_SCREEN}>
             <ProfileStack.Screen
-                name="languagepicker"
+                name={PROFILE_STACK__MAIN_PROFILE_SCREEN}
+                component={Profile}
+            />
+            <ProfileStack.Screen
+                name={PROFILE_STACK__SETTINGS_SCREEN}
+                component={Setting}
+            />
+            <ProfileStack.Screen
+                name={PROFILE_STACK__LANGUAGE_PICKER_SCREEN}
                 component={LanguagePicker}
             />
             <ProfileStack.Screen
-                name="settingsupdater"
+                name={PROFILE_STACK__SETTINGS_UPDATER_SCREEN}
                 component={SettingsUpdater}
+            />
+            <ProfileStack.Screen
+                name={PROFILE_STACK__UPDATE_PROFILE_SCREEN}
+                component={UpdateProfile}
+            />
+            <ProfileStack.Screen
+                name={PROFILE_STACK__UPDATE_SOCIAL_MEDIA_LINKS_SCREEN}
+                component={UpdateSocialMediaLinks}
             />
         </ProfileStack.Navigator>
     )
