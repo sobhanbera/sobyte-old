036904de76686d134fca15b6e44c2bca18619b14
sobhanbera <sobhanbera258@gmail.com>
2021-10-24 20:11:11 +0530

    fixed center button taking full witdth for touchability

--- app/components/CenterButtonView/index.tsx
+++ app/components/CenterButtonView/index.tsx
@@ -43,6 +43,7 @@ const styles = StyleSheet.create({
         // width: '100%',
         justifyContent: 'center',
         alignItems: 'center',
+        alignSelf: 'center', // or it will cover the full width
         paddingVertical: 15,
         paddingHorizontal: 8,
     },
